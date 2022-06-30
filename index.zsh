2cf0fd8b085d2428ac8af73b43b3875ffe2cbf5f
Quantum Snowball <quantum.snowball@gmail.com>
2022-06-30 21:28:51 +0800

    shell module refactor

--- index.zsh
+++ index.zsh
@@ -5,10 +5,7 @@ zsh_init()
 
     source $base/path/index.zsh
 
-    source $base/zsh/init.zsh
-    source $base/zsh/env.zsh
-    source $base/zsh/commands.zsh
-    source $base/zsh/utils.zsh
+    source $base/shell/index.zsh
 
     source $base/plugins/zinit/init.zsh
     source $base/plugins/zinit/loader.zsh
---
+++ shell/index.zsh
@@ -0,0 +1,12 @@
+shell_init()
+{
+    local base=$HOME/.config/zshrc/shell
+
+
+    source $base/common.zsh
+    source $base/env.zsh
+    source $base/commands.zsh
+    source $base/utils.zsh
+}
+
+shell_init
