70475a5e5f60f3b75a6bde2e8d07bfe3b168c6be
Quantum Snowball <quantum.snowball@gmail.com>
2022-06-30 22:01:03 +0800

    cf utils function can go to specific path and launch nvim directly

--- shell/utils.zsh
+++ shell/utils.zsh
@@ -1,3 +1,20 @@
 # mkdir and then cd into it in one command
-mkcd () { mkdir -p -- "$1" && cd -P -- "$1" }
+mkcd ()
+{
+    mkdir -p -- "$1" && cd -P -- "$1" 
+}
 
+cf ()
+{
+    case $1 in
+    vi | nvim | vim | neovim)
+        cd ~/.config/nvim && nvim
+        ;;
+    zshrc | shell | zsh)
+        cd ~/.config/zshrc && nvim
+        ;;
+    tmux)
+        cd ~/.config/tmux && nvim
+        ;;
+    esac
+}
