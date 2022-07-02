b6feb7e229de36dbc948fb2ad1d8b19ed9dcdc95
Quantum Snowball <quantum.snowball@gmail.com>
2022-07-03 05:57:36 +0800

    not using bare git repo to manage dotfiles anymore

--- aliases/git.zsh
+++ aliases/git.zsh
@@ -2,10 +2,3 @@
 alias gts='git status -uall'
 alias gtl='git log --oneline'
 
-# dotfiles git
-alias dot='git --git-dir=$HOME/.dot --work-tree=$HOME'
-alias dotgit='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-alias dgts='dotgit status -uall'
-alias dgtl='dotgit log --oneline'
-alias dotlazygit='lazygit --git-dir .dotfiles --work-tree ~'
-
