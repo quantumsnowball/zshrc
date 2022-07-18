0d19719258172583f3852d5f258020a360223b78
Quantum Snowball <quantum.snowball@gmail.com>
2022-07-18 17:19:59 +0800

    colored git output

--- shell/utils.zsh
+++ shell/utils.zsh
@@ -26,16 +26,32 @@ cf ()
             cd ~/.config/alacritty && nvim alacritty.yml
             ;;
         fetch | status)
+            echo "$(echo "\n# .repo\n#\n" && cd ~/.config/.repo && \
+                git -c color.ui=always fetch --all && git -c color.ui=always status --short)" &
+            echo "$(echo "\n# zshrc\n#\n" && cd ~/.config/zshrc && \
+                git -c color.ui=always fetch --all && git -c color.ui=always status --short)" &
+            echo "$(echo "\n# nvim \n#\n" && cd ~/.config/nvim  && \
+                git -c color.ui=always fetch --all && git -c color.ui=always status --short)" &
+            echo "$(echo "\n# tmux \n#\n" && cd ~/.config/tmux  && \
+                git -c color.ui=always fetch --all && git -c color.ui=always status --short)" &
+            wait
+            ;;
+        fetch-sync | status-sync)
             (echo "\n# .repo\n#\n" && cd ~/.config/.repo && git fetch --all && git status --short)
             (echo "\n# zshrc\n#\n" && cd ~/.config/zshrc && git fetch --all && git status --short)
             (echo "\n# nvim \n#\n" && cd ~/.config/nvim  && git fetch --all && git status --short)
             (echo "\n# tmux \n#\n" && cd ~/.config/tmux  && git fetch --all && git status --short)
+            echo "\n"
             ;;
         pull)
-            echo "$(echo "\n# .repo\n#\n" && cd ~/.config/.repo && git pull --rebase --autostash)" &
-            echo "$(echo "\n# zshrc\n#\n" && cd ~/.config/zshrc && git pull --rebase --autostash)" &
-            echo "$(echo "\n# nvim \n#\n" && cd ~/.config/nvim  && git pull --rebase --autostash)" &
-            echo "$(echo "\n# tmux \n#\n" && cd ~/.config/tmux  && git pull --rebase --autostash)" &
+            echo "$(echo "\n# .repo\n#\n" && cd ~/.config/.repo && \
+                git -c color.ui=always pull --rebase --autostash)" &
+            echo "$(echo "\n# zshrc\n#\n" && cd ~/.config/zshrc && \
+                git -c color.ui=always pull --rebase --autostash)" &
+            echo "$(echo "\n# nvim \n#\n" && cd ~/.config/nvim  && \
+                git -c color.ui=always pull --rebase --autostash)" &
+            echo "$(echo "\n# tmux \n#\n" && cd ~/.config/tmux  && \
+                git -c color.ui=always pull --rebase --autostash)" &
             wait
             ;;
         pull-sync)
