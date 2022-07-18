7f2527a84769f4bd5091ba2a7494f30912f70d17
Quantum Snowball <quantum.snowball@gmail.com>
2022-07-19 01:28:03 +0800

    c for clear

--- aliases/common.zsh
+++ aliases/common.zsh
@@ -1,14 +1,12 @@
 # sudo
 alias sudo='sudo '
 # clear
+alias c=clear
 alias cl=clear
-
 # reload shell
 alias rl='exec zsh'
-
 # exit
 alias x=exit
-
 # exa as ls
 alias ls='exa -F --icons'
 alias  l='exa -F --icons'
@@ -16,17 +14,13 @@ alias la='exa -aF --icons'
 alias lt='exa -aF -s time --reverse --icons'
 alias ll='exa -aF -l -h --icons --grid'
 alias lT='exa -aF -l -h --icons --tree --level=2'
-
 # directory and files
 #alias mv='mv -i'
 #alias rm='rm -i'
-
 # process
 alias jobs='jobs -l'
-
 # neovim
 alias vi='nvim'
 alias v='nvim'
-
 # youtube-dl
 alias ytdl='youtube-dl'
