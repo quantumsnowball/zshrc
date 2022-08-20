263475eb2098d1af4d846d44add218b69dc19cd8
Quantum Snowball <quantum.snowball@gmail.com>
2022-08-21 04:57:56 +0800

    fzf min-height 20 rows

--- shell/commands.zsh
+++ shell/commands.zsh
@@ -7,4 +7,4 @@ eval "$(zoxide init zsh)"
 
 # fzf
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
-export FZF_DEFAULT_OPTS='--border --layout=reverse --height=50%'
+export FZF_DEFAULT_OPTS='--border --layout=reverse --height=50% --min-height=20'
