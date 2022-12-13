1f319f32d69207809258c555cccf91ef0a9a1714
Quantum Snowball <quantum.snowball@gmail.com>
2022-12-13 18:41:51 +0800

    temp fix for iproute2mac lacking function

--- aliases/iproute2.zsh
+++ aliases/iproute2.zsh
@@ -1,8 +1,15 @@
-alias ip='ip -c'
-# list links, ip addresses, routes
-lslink() { ip -br link; }
-lsip() { ip -br addr; }
-lsip4() { ip -br -4 addr;}
-lsip6() { ip -br -6 addr;}
-lsroute() { ip route; }
-
+    # list links, ip addresses, routes
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    lslink() { ip link; }
+    lsip() { ip addr; }
+    lsip4() { ip -4 addr;}
+    lsip6() { ip -6 addr;}
+    lsroute() { ip route; }
+else
+    alias ip='ip -c'
+    lslink() { ip -br link; }
+    lsip() { ip -br addr; }
+    lsip4() { ip -br -4 addr;}
+    lsip6() { ip -br -6 addr;}
+    lsroute() { ip route; }
+fi
