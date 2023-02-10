ee4d1b52abf7481e8f39ab0d4e593da14f44b0f1
Quantum Snowball <quantum.snowball@gmail.com>
2023-02-10 17:18:35 +0800

    myip

--- aliases/iproute2.zsh
+++ aliases/iproute2.zsh
@@ -5,6 +5,7 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     lsip4() { ip -4 addr;}
     lsip6() { ip -6 addr;}
     lsroute() { ip route; }
+    myip() { ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1 }
 else
     alias ip='ip -c'
     lslink() { ip -c -br link; }
@@ -12,4 +13,5 @@ else
     lsip4() { ip -c -br -4 addr;}
     lsip6() { ip -c -br -6 addr;}
     lsroute() { ip -c route; }
+    myip() { ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1 }
 fi
