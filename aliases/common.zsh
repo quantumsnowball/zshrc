72f8a16e24f239fd257e3530e4440e57a5aba58f
Quantum Snowball <quantum.snowball@gmail.com>
2023-09-23 11:45:15 +0800

    exa is renamed to eza

--- aliases/common.zsh
+++ aliases/common.zsh
@@ -7,13 +7,13 @@ alias cl=clear
 alias rl='exec zsh'
 # exit
 alias x=exit
-# exa as ls
-alias ls='exa -F --icons'
-alias  l='exa -F --icons'
-alias la='exa -aF --icons'
-alias lt='exa -aF -s time --reverse --icons'
-alias ll='exa -aF -l -h --icons --grid'
-alias lT='exa -aF -l -h --icons --tree --level=2'
+# eza as ls
+alias ls='eza -F --icons'
+alias  l='eza -F --icons'
+alias la='eza -aF --icons'
+alias lt='eza -aF -s time --reverse --icons'
+alias ll='eza -aF -l -h --icons --grid'
+alias lT='eza -aF -l -h --icons --tree --level=2'
 # files
 #alias mv='mv -i'
 #alias rm='rm -i'
