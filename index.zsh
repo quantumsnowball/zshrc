5964b2d9df708cf6f924755095678fe3bf2c94f3
Quantum Snowball <quantum.snowball@gmail.com>
2023-12-10 19:32:04 +0800

    rft: no vars

--- index.zsh
+++ index.zsh
@@ -1,19 +1,5 @@
-init_zshrc()
-{
-    local base=$HOME/.config/zshrc
-
-
-    source $base/paths/index.zsh
-
-    source $base/init/index.zsh
-
-    source $base/utils/index.zsh
-
-    source $base/plugins/index.zsh
-
-    source $base/aliases/index.zsh
-}
-
-init_zshrc
-
-unset -f init_zshrc
+source $HOME/.config/zshrc/paths/index.zsh
+source $HOME/.config/zshrc/init/index.zsh
+source $HOME/.config/zshrc/utils/index.zsh
+source $HOME/.config/zshrc/plugins/index.zsh
+source $HOME/.config/zshrc/aliases/index.zsh
