5c87ecaa37154475c438ff7f53e81520be50c9a2
Quantum Snowball <quantum.snowball@gmail.com>
2023-12-10 19:35:26 +0800

    rft: no vars aliases

--- aliases/index.zsh
+++ aliases/index.zsh
@@ -1,29 +1,18 @@
-init_aliases() 
-{
-    local base=$HOME/.config/zshrc/aliases
-    
-
-    source $base/shell.zsh
-    source $base/dir.zsh
-    source $base/eza.zsh
-    source $base/nvim.zsh
-    source $base/tmux.zsh
-    source $base/docker.zsh
-    source $base/rclone.zsh
-    source $base/lazygit.zsh
-    source $base/env.zsh
-    source $base/git.zsh
-    source $base/python.zsh
-    source $base/zoxide.zsh
-    source $base/iproute2.zsh
-    source $base/socket.zsh
-    source $base/youtube-dl.zsh
-
-    if [ -f "/etc/arch-release" ]; then 
-        source $base/pacman.zsh
-    fi
-}
-
-init_aliases
-
-unset -f init_aliases
+source $HOME/.config/zshrc/aliases/shell.zsh
+source $HOME/.config/zshrc/aliases/dir.zsh
+source $HOME/.config/zshrc/aliases/eza.zsh
+source $HOME/.config/zshrc/aliases/nvim.zsh
+source $HOME/.config/zshrc/aliases/tmux.zsh
+source $HOME/.config/zshrc/aliases/docker.zsh
+source $HOME/.config/zshrc/aliases/rclone.zsh
+source $HOME/.config/zshrc/aliases/lazygit.zsh
+source $HOME/.config/zshrc/aliases/env.zsh
+source $HOME/.config/zshrc/aliases/git.zsh
+source $HOME/.config/zshrc/aliases/python.zsh
+source $HOME/.config/zshrc/aliases/zoxide.zsh
+source $HOME/.config/zshrc/aliases/iproute2.zsh
+source $HOME/.config/zshrc/aliases/socket.zsh
+source $HOME/.config/zshrc/aliases/youtube-dl.zsh
+if [ -f "/etc/arch-release" ]; then 
+    source $HOME/.config/zshrc/aliases/pacman.zsh
+fi
