22fda3b0eaf662476b8f5c67fb07e0f65ae38958
Quantum Snowball <quantum.snowball@gmail.com>
2023-12-11 05:50:58 +0800

    pip-upgrade-all

--- utils/__init__.zsh
+++ utils/__init__.zsh
@@ -3,5 +3,6 @@ source $HOME/.config/zshrc/utils/config.zsh
 source $HOME/.config/zshrc/utils/zoxide.zsh
 source $HOME/.config/zshrc/utils/python.zsh
 source $HOME/.config/zshrc/utils/conda/__init__.zsh
+source $HOME/.config/zshrc/utils/pip.zsh
 source $HOME/.config/zshrc/utils/memory.zsh
 source $HOME/.config/zshrc/utils/ffmpeg/__init__.zsh
---
+++ utils/pip.zsh
@@ -0,0 +1,32 @@
+pip-upgrade-all() 
+{
+  # List outdated packages
+  outdated_packages=$(pip list --outdated)
+
+  # Check if there are any outdated packages
+  if [[ -z $outdated_packages ]]; then
+    echo "\nNo outdated packages found\n"
+    return
+  fi
+
+  # Display the outdated packages
+  echo "\nOutdated packages:\n"
+  echo "$outdated_packages"
+
+  # Prompt for user confirmation
+  echo "\n"
+  read -q "REPLY?Do you want to upgrade these packages? (y/[N]) "
+
+  echo
+
+  # Upgrade packages if confirmed
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    echo "\nUpgrading packages..."
+    echo
+
+    # Extract the package names and upgrade each package
+    echo "$outdated_packages" | awk '{print $1}' | xargs -n1 pip install --upgrade
+  else
+    echo "\nPackage upgrade canceled\n"
+  fi
+}
