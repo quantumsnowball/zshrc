e341af8e7cfb4068fbc7f3344a9d83b606afdee0
Quantum Snowball <quantum.snowball@gmail.com>
2023-12-10 19:44:53 +0800

    rft: index -> __init__

--- index.zsh
+++ index.zsh
@@ -1,5 +1,5 @@
-source $HOME/.config/zshrc/paths/index.zsh
-source $HOME/.config/zshrc/init/index.zsh
-source $HOME/.config/zshrc/utils/index.zsh
-source $HOME/.config/zshrc/plugins/index.zsh
-source $HOME/.config/zshrc/aliases/index.zsh
+source $HOME/.config/zshrc/paths/__init__.zsh
+source $HOME/.config/zshrc/init/__init__.zsh
+source $HOME/.config/zshrc/utils/__init__.zsh
+source $HOME/.config/zshrc/plugins/__init__.zsh
+source $HOME/.config/zshrc/aliases/__init__.zsh
