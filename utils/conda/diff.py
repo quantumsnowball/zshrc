1feac5db0c96ff38913b3a2fc6887785354a9d4b
Quantum Snowball <quantum.snowball@gmail.com>
2023-12-11 04:46:42 +0800

    pass args

--- utils/conda/diff.py
+++ utils/conda/diff.py
@@ -1,1 +1,16 @@
-print('conda-diff')
+import sys
+
+# help
+usage = 'Usage: conda-diff ENV1 ENV2'
+
+# args
+args = sys.argv[1:]
+
+# validate inputs
+if len(args) != 2:
+    print(usage)
+    sys.exit(1)
+
+env1, env2 = args
+
+print(env1, env2)
