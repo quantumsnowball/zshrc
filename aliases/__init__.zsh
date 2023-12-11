c51dee2f02ee453b5756e5f0002382c9b91e826d
Quantum Snowball <quantum.snowball@gmail.com>
2023-12-11 23:46:06 +0800

    mv zoxide

--- aliases/__init__.zsh
+++
@@ -1,1 +0,0 @@
-source $HOME/.config/zshrc/aliases/zoxide.zsh
--- utils/zoxide.zsh
+++ utils/zoxide.zsh
@@ -1,3 +1,10 @@
+# zoxide
+# alias z="__zoxide_z" # disabled: this breaks z <Tab> completion
+alias zz="__zoxide_zi"
+alias Z="__zoxide_zi"
+alias zl="z -"
+
+
 # z then v
 zv() {
   z $1 && nvim;
