47c4847c2eca17660f2d86c0c9c07b90daaabad1
Quantum Snowball <quantum.snowball@gmail.com>
2023-12-12 01:17:48 +0800

    better wtfls, avoided using which, use command -v instead

--- utils/help.zsh
+++ utils/help.zsh
@@ -1,10 +1,10 @@
 wtfis()
 {
-    echo -e "\nwhich $1:"
-    which $1
-    echo -e "\ntype $1:"
-    type $1
-    echo -e "\nwhatis $1:"
+    echo -e "\n>_ command -v $1"
+    command -v $1
+    echo -e "\n>_ type -a $1"
+    type -a $1
+    echo -e "\n>_ whatis $1"
     whatis $1
     echo -e "\n"
 }
