e35755492525643a848f268f92576a4277f04d9b
Quantum Snowball <quantum.snowball@gmail.com>
2023-12-12 00:29:38 +0800

    no cond in __init__

--- paths/__init__.zsh
+++ paths/__init__.zsh
@@ -1,5 +1,3 @@
 source $HOME/.config/zshrc/paths/local.zsh
 source $HOME/.config/zshrc/paths/yarn.zsh
-if [[ "$OSTYPE" == "darwin"* ]]; then 
-    source $HOME/.config/zshrc/paths/homebrew.zsh
-fi
+source $HOME/.config/zshrc/paths/homebrew.zsh
