20b1067a93ee06a3151f2442f58fcf50935be01a
Quantum Snowball <quantum.snowball@gmail.com>
2023-12-11 23:58:05 +0800

    utils comes after plugins to allow redefining aliases

--- __init__.zsh
+++ __init__.zsh
@@ -1,4 +1,4 @@
 source $HOME/.config/zshrc/paths/__init__.zsh
 source $HOME/.config/zshrc/init/__init__.zsh
-source $HOME/.config/zshrc/utils/__init__.zsh
 source $HOME/.config/zshrc/plugins/__init__.zsh
+source $HOME/.config/zshrc/utils/__init__.zsh
