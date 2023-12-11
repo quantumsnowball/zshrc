6d948b3c0fecb77d60d60ab7fd437abe1ef6b5cc
Quantum Snowball <quantum.snowball@gmail.com>
2023-12-11 23:46:51 +0800

    rmdir aliases

--- __init__.zsh
+++ __init__.zsh
@@ -1,5 +1,4 @@
 source $HOME/.config/zshrc/paths/__init__.zsh
 source $HOME/.config/zshrc/init/__init__.zsh
 source $HOME/.config/zshrc/utils/__init__.zsh
-source $HOME/.config/zshrc/aliases/__init__.zsh
 source $HOME/.config/zshrc/plugins/__init__.zsh
