24209ea82651db2015dc921716917d5060bd471a
Quantum Snowball <quantum.snowball@gmail.com>
2023-12-11 23:51:32 +0800

    reorder

--- utils/__init__.zsh
+++ utils/__init__.zsh
@@ -1,21 +1,23 @@
-source $HOME/.config/zshrc/utils/dir.zsh
-source $HOME/.config/zshrc/utils/config.zsh
-source $HOME/.config/zshrc/utils/zoxide.zsh
-source $HOME/.config/zshrc/utils/python.zsh
 source $HOME/.config/zshrc/utils/conda/__init__.zsh
-source $HOME/.config/zshrc/utils/pip.zsh
-source $HOME/.config/zshrc/utils/memory.zsh
 source $HOME/.config/zshrc/utils/ffmpeg/__init__.zsh
-source $HOME/.config/zshrc/utils/rclone.zsh
-source $HOME/.config/zshrc/utils/youtube-dl.zsh
+
+source $HOME/.config/zshrc/utils/config.zsh
+source $HOME/.config/zshrc/utils/dir.zsh
 source $HOME/.config/zshrc/utils/docker.zsh
+source $HOME/.config/zshrc/utils/env.zsh
 source $HOME/.config/zshrc/utils/eza.zsh
 source $HOME/.config/zshrc/utils/git.zsh
-source $HOME/.config/zshrc/utils/nvim.zsh
 source $HOME/.config/zshrc/utils/iproute2.zsh
-source $HOME/.config/zshrc/utils/tmux.zsh
 source $HOME/.config/zshrc/utils/lazygit.zsh
-source $HOME/.config/zshrc/utils/socket.zsh
-source $HOME/.config/zshrc/utils/shell.zsh
-source $HOME/.config/zshrc/utils/env.zsh
+source $HOME/.config/zshrc/utils/memory.zsh
+source $HOME/.config/zshrc/utils/nvim.zsh
 source $HOME/.config/zshrc/utils/pacman.zsh
+source $HOME/.config/zshrc/utils/pip.zsh
+source $HOME/.config/zshrc/utils/python.zsh
+source $HOME/.config/zshrc/utils/rclone.zsh
+source $HOME/.config/zshrc/utils/shell.zsh
+source $HOME/.config/zshrc/utils/socket.zsh
+source $HOME/.config/zshrc/utils/tmux.zsh
+source $HOME/.config/zshrc/utils/youtube-dl.zsh
+source $HOME/.config/zshrc/utils/zoxide.zsh
+
