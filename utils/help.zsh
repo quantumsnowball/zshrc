17e6c6e946e012fcc6fbf7c0121c47b0ea9b4f31
Quantum Snowball <quantum.snowball@gmail.com>
2023-12-12 01:34:16 +0800

    wtfis color output

--- utils/help.zsh
+++ utils/help.zsh
@@ -1,10 +1,15 @@
 wtfis()
 {
-    echo -e "\n>_ command -v $1"
+    # Define color escape sequences
+    G="\033[32m"
+    B="\033[34m"
+    rs="\033[0m"
+
+    echo -e "\n>_ ${G}command -v${rs} ${B}$1${rs}"
     command -v $1
-    echo -e "\n>_ type -a $1"
+    echo -e "\n>_ ${G}type -a${rs} ${B}$1${rs}"
     type -a $1
-    echo -e "\n>_ whatis $1"
+    echo -e "\n>_ ${G}whatis${rs} ${B}$1${rs}"
     whatis $1
     echo -e "\n"
 }
