ff27aa97202860a1255f98bfc7908afb35f283cc
Quantum Snowball <quantum.snowball@gmail.com>
2023-12-12 21:44:16 +0800

    step: some basic info displayed

--- utils/ffprobe/streams.py
+++ utils/ffprobe/streams.py
@@ -1,6 +1,7 @@
 import sys
 import subprocess
 import json
+from typing import Any
 
 
 # help
@@ -28,14 +29,23 @@ cmd = ('ffprobe',
 # parse output
 stdout = subprocess.check_output(cmd)
 data = json.loads(stdout.decode())
-streams: list[dict[str, str]] = data['streams']
+streams: list[dict[str, Any]] = data['streams']
+
 
 # display streams
-
-
-def display(s: dict[str, str]) -> None:
-    print(s['codec_name'])
-    print(s['codec_type'])
+def display(s: dict[str, Any]) -> None:
+    idx = s['index']
+    name = s['codec_name']
+    long_name = s['codec_long_name']
+    kind = s['codec_type']
+    print(f'Stream {idx}: {kind}')
+    print(f'\t{name} ({long_name})')
+    if kind == 'video':
+        print(f"\t{s['width']} x {s['height']} px")
+    elif kind == 'audio':
+        print(f"\t{float(s['bit_rate'])/1e3} kb/s (lang: {s['tags']['language']})")
+    else:
+        print(f"\t")
 
 
 for s in streams:
