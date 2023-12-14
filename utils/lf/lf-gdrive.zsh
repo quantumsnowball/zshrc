0f327fef983861cd6e04a3dd074b8ada1ae3985b
Quantum Snowball <quantum.snowball@gmail.com>
2023-12-14 15:14:55 +0800

    essential working

--- utils/lf/lf-gdrive.zsh
+++ utils/lf/lf-gdrive.zsh
@@ -1,7 +1,20 @@
 lf-gdrive () {
-    mkdir -p $HOME/gdrive/$1
+    # define vars
+    root=gdrive
+    target=$HOME/$root/$1
 
-    
+    # ensure dir
+    mkdir -p $target
+
+    # start mount
+    rclone mount --daemon $1 $target &
+    sleep 5
+
+    # start browsing
+    command lf $target
+
+    # unmount when finished
+    fusermount -u $target
 }
 
 alias lfg=lf-gdrive
