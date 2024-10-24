81747afaaf224fc90fb956106524e1e43f497a79
Quantum Snowball <quantum.snowball@gmail.com>
2024-10-24 22:45:05 +0800

    aptgrep

--- utils/apt.zsh
+++ utils/apt.zsh
@@ -2,6 +2,9 @@
 if command -v apt &> /dev/null; then 
     alias apti='sudo apt install'
     alias apts='apt search'
+    alias aptls='apt list'
+    alias aptgrep='apt list | rg'
+    alias aptrg=aptgrep
     alias aptrm='sudo apt remove'
     alias aptu='sudo apt update'
     alias aptup='sudo apt update && sudo apt upgrade'
