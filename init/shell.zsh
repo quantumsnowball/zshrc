f40f2a14b3103c85353d4354a753489542054de6
Quantum Snowball <quantum.snowball@gmail.com>
2024-11-01 22:27:47 +0800

    Fix: 'zvm_cursor_style:33: failed to compile regex: trailing backslash (\)' on every command entered on Termux

--- init/shell.zsh
+++ init/shell.zsh
@@ -1,3 +1,7 @@
 # disable ctrl-d quitting shell directly
 # setopt IGNORE_EOF
 
+# Fix: 'zvm_cursor_style:33: failed to compile regex: trailing backslash (\)' on every command entered on Termux
+# From github issue solution: fix On macOS (homebrew) and Linux (DNF/Fedora), zsh supports PCRE by default, 
+# apparently this issue will only occur on FreeBSD using pre-compiled packages. 
+setopt re_match_pcre
