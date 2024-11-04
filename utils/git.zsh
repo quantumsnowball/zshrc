ad3f536ad6c7397c1a9fe5283922b9a2d864336a
Quantum Snowball <quantum.snowball@gmail.com>
2024-11-04 19:48:28 +0800

    comment

--- utils/git.zsh
+++ utils/git.zsh
@@ -9,16 +9,24 @@ alias gtl='git log --oneline'
 
 #helpers
 clone-my-repo() {
+    # default use https url
     local use_ssh=false
-    if [ "$1" = "-rw" ] || [ "$1" = "-ssh" ]; then
+
+    # if -rw or -ssh flag active, use ssh url
+    # only machine with authorized private key installed can write to repo
+    if [ "$1" = "-ssh" ] || [ "$1" = "-rw" ]; then
         use_ssh=true
         shift
     fi
+
+    # the arg that follows is always the repo name
     if [ -z "$1" ]; then
-        echo "Usage: clone-my-repo [-ssh] <repo> [<git-args>]"
+        echo "Usage: clone-my-repo [-ssh|-rw] <repo> [<git clone args>]"
         return 1
     fi
     local repo="$1"
+
+    # after the repo name, all following args ($@) are passed onto git clone
     shift
     if $use_ssh; then
         # echo "git clone $myghssh/$repo.git $@"
