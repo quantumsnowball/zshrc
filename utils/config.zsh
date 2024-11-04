7822f8fa831d650716e35b2d99ba15cde49bc8c5
Quantum Snowball <quantum.snowball@gmail.com>
2024-11-05 03:15:56 +0800

    cf status

--- utils/config.zsh
+++ utils/config.zsh
@@ -5,6 +5,14 @@ cf ()
     # - can retain current directory
     # - can avoid printing the & [1] <PID> and + done debug message 
     (
+        status ()
+        {
+            echo "$(
+                echo "\n# $1\n#\n" &&
+                cd ~/.config/$1 &&
+                git -c color.ui=always status --short --branch 2>&1
+            )"
+        }
         pull ()
         {
             echo "$(
@@ -13,7 +21,16 @@ cf ()
                 git -c color.ui=always pull --rebase --autostash 2>&1
             )"
         }
+
+
         case $1 in
+        status)
+            status settings & 
+            status zshrc & 
+            status nvim & 
+            status tmux & 
+            wait
+            ;;
         pull)
             pull settings & 
             pull zshrc & 
