64d83d71d447981c758633f1ce38b54e7cde7fd1
Quantum Snowball <quantum.snowball@gmail.com>
2024-11-05 03:31:52 +0800

    formatting

--- utils/config.zsh
+++ utils/config.zsh
@@ -18,7 +18,8 @@ cf ()
             echo "$(
                 echo "\n#\n# < $1 >\n#" &&
                 cd ~/.config/$1 &&
-                git -c color.ui=always fetch --all && git -c color.ui=always status --short --branch 2>&1
+                git -c color.ui=always fetch --all && 
+                git -c color.ui=always status --short --branch 2>&1
             )"
         }
         pull ()
