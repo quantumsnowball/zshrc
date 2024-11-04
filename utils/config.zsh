bd380f4cd1ee14ac7632adb6dc473e68a43c1e43
Quantum Snowball <quantum.snowball@gmail.com>
2024-11-05 03:02:59 +0800

    dummy example running long process in parallel

--- utils/config.zsh
+++ utils/config.zsh
@@ -1,104 +1,23 @@
 # nvim editor config repos directly
 cf ()
 {
-    # run in a subshell to retain current directory
+    # run in a subshell
+    # - can retain current directory
+    # - can avoid printing the & [1] <PID> and + done debug message 
     (
-        cf status ()
+        pull ()
         {
-            echo "echo \"\n# $1\n#\n\" && cd ~/.config/$1 && \
-                git -c color.ui=always status --short --branch"
+            echo "$(
+                echo "cf pull $1 started\n"
+                sleep 5                    
+                echo "cf pull $1 finished\n"
+            )"
         }
-
-        cf fetch ()
-        {
-            echo "echo \"\n# $1\n#\n\" && cd ~/.config/$1 && \
-                git -c color.ui=always fetch --all && git -c color.ui=always status --short --branch"
-        }
-
-        cf pull ()
-        {
-            echo "echo \"\n# $1\n#\n\" && cd ~/.config/$1 && \
-                git -c color.ui=always pull --rebase --autostash"
-        }
-
-        cf push ()
-        {
-            echo "echo \"\n# $1\n#\n\" && cd ~/.config/$1 && \
-                git -c color.ui=always push"
-        }
-
-        cf sync ()
-        {
-            echo "echo \"\n# $1\n#\n\" && cd ~/.config/$1 && \
-                git pull --rebase --autostash --quiet && git push --quiet && git -c color.ui=always status --short --branch"
-        }
-
         case $1 in
-        set | setting | settings | conf | config)
-            cd ~/.config/settings/ && nvim
-            ;;
-        vi | nvim | vim | neovim)
-            cd ~/.config/nvim && nvim
-            ;;
-        zshrc | shell | zsh)
-            cd ~/.config/zshrc && nvim
-            ;;
-        poshrc | posh)
-            cd ~/winhome/Documents/WindowsPowerShell/poshrc && nvim
-            ;;
-        tmux)
-            cd ~/.config/tmux && nvim
-            ;;
-        ala | alacritty)
-            cd ~/.config/alacritty && nvim alacritty.yml
-            ;;
-        status)
-            echo "$(eval "$(status settings)")" &
-            echo "$(eval "$(status zshrc)")" &
-            echo "$(eval "$(status nvim )")" &
-            echo "$(eval "$(status tmux )")" &
-            wait
-            ;;
-        fetch)
-            echo "$(eval "$(fetch settings)")" &
-            echo "$(eval "$(fetch zshrc)")" &
-            echo "$(eval "$(fetch nvim )")" &
-            echo "$(eval "$(fetch tmux )")" &
-            wait
-            ;;
-        fetch-sync | status-sync)
-            (eval "$(fetch settings)")
-            (eval "$(fetch zshrc)")
-            (eval "$(fetch nvim)")
-            (eval "$(fetch tmux)")
-            echo "\n"
-            ;;
         pull)
-            echo "$(eval "$(pull settings)")" &
-            echo "$(eval "$(pull zshrc)")" &
-            echo "$(eval "$(pull nvim )")" &
-            echo "$(eval "$(pull tmux )")" &
-            wait
-            ;;
-        pull-sync)
-            (eval "$(pull settings)")
-            (eval "$(pull zshrc)")
-            (eval "$(pull nvim)")
-            (eval "$(pull tmux)")
-            echo "\n"
-            ;;
-        push)
-            echo "$(eval "$(push settings)")" &
-            echo "$(eval "$(push zshrc)")" &
-            echo "$(eval "$(push nvim )")" &
-            echo "$(eval "$(push tmux )")" &
-            wait
-            ;;
-        sync)
-            echo "$(eval "$(sync settings)")" &
-            echo "$(eval "$(sync zshrc)")" &
-            echo "$(eval "$(sync nvim )")" &
-            echo "$(eval "$(sync tmux )")" &
+            pull arg1 & 
+            pull arg2 & 
+            pull arg3 & 
             wait
             ;;
         esac
