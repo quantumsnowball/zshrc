b9889403669ecae391754a2346b6939f5e47156a
Quantum Snowball <quantum.snowball@gmail.com>
2024-11-04 19:57:21 +0800

    check for invalid flag

--- utils/git.zsh
+++ utils/git.zsh
@@ -11,23 +11,29 @@ alias gtl='git log --oneline'
 clone-my-repo() {
     # default use https url
     local use_ssh=false
+    local help="Usage: clone-my-repo [-ssh|-rw] <repo> [<git clone args>]"
 
     # if -rw or -ssh flag active, use ssh url
     # only machine with authorized private key installed can write to repo
     if [ "$1" = "-ssh" ] || [ "$1" = "-rw" ]; then
         use_ssh=true
         shift
+    elif [[ "$1" == -* ]]; then
+        echo "Error: invalid flag"
+        echo $help
+        return 1 
     fi
 
     # the arg that follows is always the repo name
     if [ -z "$1" ]; then
-        echo "Usage: clone-my-repo [-ssh|-rw] <repo> [<git clone args>]"
+        echo "Error: please provide repo name to clone"
+        echo $help
         return 1
     fi
     local repo="$1"
+    shift
 
     # after the repo name, all following args ($@) are passed onto git clone
-    shift
     if $use_ssh; then
         # echo "git clone $myghssh/$repo.git $@"
         git clone "$myghssh/$repo.git" "$@"
