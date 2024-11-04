875e64b9689f281bdad7bc84df5bb2f778d4b7c7
Quantum Snowball <quantum.snowball@gmail.com>
2024-11-04 19:36:56 +0800

    -ssh flag

--- utils/git.zsh
+++ utils/git.zsh
@@ -1,16 +1,30 @@
 # constant
 export gh=https://github.com
-export mygh=$gh/quantumsnowball
+export myghname=quantumsnowball
+export mygh=$gh/$myghname
+export myghssh=git@github.com:$myghname
 # git
 alias gts='git status -uall'
 alias gtl='git log --oneline'
 
 #helpers
 clone-my-repo() {
+    local use_ssh=false
+    if [ "$1" = "-rw" ] || [ "$1" = "-ssh" ]; then
+        use_ssh=true
+        shift
+    fi
     if [ -z "$1" ]; then
-        echo "Usage: clone-my-repo <repo>"
+        echo "Usage: clone-my-repo [-ssh] <repo> [<git-args>]"
         return 1
     fi
-    # git clone
-    git clone "$mygh/$1.git"
+    local repo="$1"
+    shift
+    if $use_ssh; then
+        # echo "git clone $myghssh/$repo.git $@"
+        git clone "$myghssh/$repo.git" "$@"
+    else
+        # echo "git clone $mygh/$repo.git $@"
+        git clone "$mygh/$repo.git" "$@"
+    fi
 }
