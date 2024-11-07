fcd6e50729f174ffb2e278f43ffc3a327bab4a3d
Quantum Snowball <quantum.snowball@gmail.com>
2024-11-08 02:55:08 +0800

    check exe first

--- utils/pip.zsh
+++ utils/pip.zsh
@@ -1,47 +1,48 @@
-alias pip-update-all=pip-upgrade-all
+if command -v pip &> /dev/null; then 
+  alias pip-update-all=pip-upgrade-all
 
-pip-install-basic()
-{
-  pip install \
-    numpy pandas matplotlib scipy \
-    ipython jupyter \
-    requests aiohttp \
-    click \
-    pytest \
-    mypy \
-    autopep8 isort
-}
+  pip-install-basic()
+  {
+    pip install \
+      numpy pandas matplotlib scipy \
+      ipython jupyter \
+      requests aiohttp \
+      click \
+      pytest \
+      mypy \
+      autopep8 isort
+  }
 
-pip-upgrade-all() 
-{
-  # List outdated packages
-  outdated_packages=$(pip list --outdated)
+  pip-upgrade-all() 
+  {
+    # List outdated packages
+    outdated_packages=$(pip list --outdated)
 
-  # Check if there are any outdated packages
-  if [[ -z $outdated_packages ]]; then
-    echo "\nNo outdated packages found\n"
-    return
-  fi
+    # Check if there are any outdated packages
+    if [[ -z $outdated_packages ]]; then
+      echo "\nNo outdated packages found\n"
+      return
+    fi
 
-  # Display the outdated packages
-  echo "\nOutdated packages:\n"
-  echo "$outdated_packages"
+    # Display the outdated packages
+    echo "\nOutdated packages:\n"
+    echo "$outdated_packages"
 
-  # Prompt for user confirmation
-  echo "\n"
-  read -q "REPLY?Do you want to upgrade these packages? (y/[N]) "
+    # Prompt for user confirmation
+    echo "\n"
+    read -q "REPLY?Do you want to upgrade these packages? (y/[N]) "
 
-  echo
-
-  # Upgrade packages if confirmed
-  if [[ $REPLY =~ ^[Yy]$ ]]; then
-    echo "\nUpgrading packages..."
     echo
 
-    # Extract the package names and upgrade each package
-    echo "$outdated_packages" | awk '{print $1}' | xargs -n1 pip install --upgrade
-  else
-    echo "\nPackage upgrade canceled\n"
-  fi
-}
+    # Upgrade packages if confirmed
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+      echo "\nUpgrading packages..."
+      echo
 
+      # Extract the package names and upgrade each package
+      echo "$outdated_packages" | awk '{print $1}' | xargs -n1 pip install --upgrade
+    else
+      echo "\nPackage upgrade canceled\n"
+    fi
+  }
+fi
