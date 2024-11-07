db8f5d5634b7a19951bcbdafd17741e19acf52d4
Quantum Snowball <quantum.snowball@gmail.com>
2024-11-07 15:46:22 +0800

    figure out the SHORT hostname

--- init/ssh.zsh
+++ init/ssh.zsh
@@ -1,4 +1,15 @@
 # omz ssh-agent plugin should already started a ssh-agent process
 # it should ask for password the first time
+
+
 #
 # put ssh-agent settings here
+#
+
+# Figure out the SHORT hostname
+if [[ "$OSTYPE" = darwin* ]]; then
+  # macOS's $HOST changes with dhcp, etc. Use ComputerName if possible.
+  SHORT_HOST=$(scutil --get ComputerName 2>/dev/null) || SHORT_HOST="${HOST/.*/}"
+else
+  SHORT_HOST="${HOST/.*/}"
+fi
