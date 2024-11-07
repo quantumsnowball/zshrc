8f57c9afb40f5fb75a206cd1ed4923c1c8eb11f4
Quantum Snowball <quantum.snowball@gmail.com>
2024-11-07 22:38:21 +0800

    conda-install-essentials

--- utils/conda/__init__.zsh
+++ utils/conda/__init__.zsh
@@ -11,4 +11,24 @@ if command -v conda &> /dev/null; then
     alias condaenvls='conda env list'
     condadiff() { python $HOME/.config/zshrc/utils/conda/diff.py "$@" | bat }
 
+    # helpers
+    conda-install-essentials () {
+        # general
+        conda install --yes click 
+        conda install --yes ipython 
+        conda install --yes jupyter 
+        conda install --yes pytest
+        # numeric
+        conda install --yes numpy
+        conda install --yes scipy 
+        conda install --yes pandas
+        # charting
+        conda install --yes matplotlib
+        # networking
+        conda install --yes requests 
+        conda install --yes aiohttp
+        # linting
+        conda install --yes mypy 
+        conda install --yes isort
+    }
 fi
