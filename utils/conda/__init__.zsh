7812150cb46a1c39746a0ec33b6c558c2184f150
Quantum Snowball <quantum.snowball@gmail.com>
2024-11-07 20:33:08 +0800

    conda helpers

--- utils/conda/__init__.zsh
+++ utils/conda/__init__.zsh
@@ -1,4 +1,15 @@
-alias lsconda='conda env list'
+if command -v conda &> /dev/null; then 
+    alias condaa='conda activate'
+    alias condai='conda install'
+    alias condas='conda search'
+    alias condals='conda list | bat'
+    alias condagrep='conda list | rg'
+    alias condarg=condagrep
+    alias condarm='conda remove'
+    alias condau='conda upgrade --all'
+    alias condaup=condau
+    alias condaenvls='conda env list'
+fi
 
 
 source $HOME/.config/zshrc/utils/conda/diff.zsh
