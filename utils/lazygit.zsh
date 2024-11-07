d47e2d0333c70c5d8b3f8f3369e50fdf10143110
Quantum Snowball <quantum.snowball@gmail.com>
2024-11-08 02:36:44 +0800

    lg nvim

--- utils/lazygit.zsh
+++ utils/lazygit.zsh
@@ -1,3 +1,17 @@
 # lazygit
-alias lg='lazygit'
-
+lg () {
+    # run in a subshell
+    # - can retain current directory
+    (
+        # Define the base directory mapping
+        local dir
+        case $1 in
+            vi | nvim | vim | neovim)
+                cd "$HOME/.config/nvim" && shift && lazygit $@
+                ;;
+            *)
+                lazygit
+                ;;
+        esac
+    )
+}
