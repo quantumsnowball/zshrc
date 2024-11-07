7d48dce27e6799b816d3e3d75312a47f140bdaa6
Quantum Snowball <quantum.snowball@gmail.com>
2024-11-08 02:39:27 +0800

    lg others

--- utils/lazygit.zsh
+++ utils/lazygit.zsh
@@ -6,8 +6,20 @@ lg () {
         # Define the base directory mapping
         local dir
         case $1 in
+            set | setting | settings | conf | config)
+                cd "$HOME/.config/settings" && shift && lazygit $@
+                ;;
             vi | nvim | vim | neovim)
                 cd "$HOME/.config/nvim" && shift && lazygit $@
+                ;;
+            zshrc | shell | zsh)
+                cd "$HOME/.config/zshrc" && shift && lazygit $@
+                ;;
+            poshrc | posh)
+                cd "$HOME/.config/poshrc" && shift && lazygit $@
+                ;;
+            tmux)
+                cd "$HOME/.config/tmux" && shift && lazygit $@
                 ;;
             *)
                 lazygit
