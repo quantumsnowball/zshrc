8c73aeeffd7482b32f88bf972d693a1bc28df6a2
Quantum Snowball <quantum.snowball@gmail.com>
2025-02-01 01:28:24 +0800

    stop accident : to trigger 'execute-named-cmd'

--- init/shell.zsh
+++ init/shell.zsh
@@ -5,3 +5,6 @@
 # From github issue solution: fix On macOS (homebrew) and Linux (DNF/Fedora), zsh supports PCRE by default, 
 # apparently this issue will only occur on FreeBSD using pre-compiled packages. 
 setopt re_match_pcre
+
+# stop accident : to trigger 'execute-named-cmd'
+bindkey -a -r ':'
