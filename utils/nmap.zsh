c95ab4027d065e133dabe064b0f93af5d1d6cc01
Quantum Snowball <quantum.snowball@gmail.com>
2025-02-12 06:04:35 +0800

    with dns servers at router

--- utils/nmap.zsh
+++ utils/nmap.zsh
@@ -1,7 +1,7 @@
 # show most connected hosts in LAN
 nmap-ls() {
     printf "\033[33m%-15s %s\033[0m\n" 'IP Address' 'Name'
-    nmap -sL 192.168.1.0/24 \
+    nmap -sL 192.168.1.0/24 --dns-servers 192.168.1.1 \
         | grep "^Nmap scan report for" \
         | awk '{print $5, $(NF)}' \
         | grep -v '^192\.168' \
