430d5289c7c2cd07f2f984fa36835715c7d29235
Quantum Snowball <quantum.snowball@gmail.com>
2025-02-12 05:51:47 +0800

    colored output

--- utils/nmap.zsh
+++ utils/nmap.zsh
@@ -1,9 +1,9 @@
 # show most connected hosts in LAN
 nmap-ls() {
-    printf "%-15s %s\n" 'IP Address' 'Name'
+    printf "\033[33m%-15s %s\033[0m\n" 'IP Address' 'Name'
     nmap -sL 192.168.1.0/24 \
         | grep "^Nmap scan report for" \
         | awk '{print $5, $(NF)}' \
         | grep -v '^192\.168' \
-        | awk '{gsub(/[()]/, ""); printf "%-15s %s\n", $2, $1}'
+        | awk '{gsub(/[()]/, ""); printf "\033[32m%-15s\033[0m %s\n", $2, $1}'
 }
