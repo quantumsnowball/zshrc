fae4ce9c5fa904a2c3eab6a0be770b17d8067089
Quantum Snowball <quantum.snowball@gmail.com>
2025-02-12 06:11:08 +0800

    nmap-ls-ping

--- utils/nmap.zsh
+++ utils/nmap.zsh
@@ -1,7 +1,16 @@
-# show most connected hosts in LAN
+# show most known hosts in LAN
 nmap-ls() {
     printf "\033[33m%-15s %s\033[0m\n" 'IP Address' 'Name'
     nmap -sL 192.168.1.0/24 --dns-servers 192.168.1.1 \
+        | grep "^Nmap scan report for" \
+        | awk '{print $5, $(NF)}' \
+        | grep -v '^192\.168' \
+        | awk '{gsub(/[()]/, ""); printf "\033[32m%-15s\033[0m %s\n", $2, $1}'
+}
+# show most connected hosts in LAN using ping scan
+nmap-ls-ping() {
+    printf "\033[33m%-15s %s\033[0m\n" 'IP Address' 'Name'
+    nmap -sn 192.168.1.0/24 --dns-servers 192.168.1.1 \
         | grep "^Nmap scan report for" \
         | awk '{print $5, $(NF)}' \
         | grep -v '^192\.168' \
