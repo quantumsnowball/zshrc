ddae7aaab9207ed23f539984444bd4396e7011a3
Quantum Snowball <quantum.snowball@gmail.com>
2025-02-16 01:40:03 +0800

    condi alias for ls

--- utils/eza.zsh
+++ utils/eza.zsh
@@ -1,9 +1,10 @@
 # eza as ls
-alias ls='eza -F --icons'
-alias  l='eza -F --icons'
-alias la='eza -aF --icons'
-alias lt='eza -aF -s time --reverse --icons'
-alias ll='eza -aF -l -h --icons'
-alias lll='eza -aF -l -h --icons --grid'
-alias lT='eza -aF -l -h --icons --tree --level=2'
-
+if command -v eza &> /dev/null; then
+    alias ls='eza -F --icons'
+    alias  l='eza -F --icons'
+    alias la='eza -aF --icons'
+    alias lt='eza -aF -s time --reverse --icons'
+    alias ll='eza -aF -l -h --icons'
+    alias lll='eza -aF -l -h --icons --grid'
+    alias lT='eza -aF -l -h --icons --tree --level=2'
+fi
