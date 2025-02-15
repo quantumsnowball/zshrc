3b4f4b963c26f33221e7b5b97a101c7e843c9d27
Quantum Snowball <quantum.snowball@gmail.com>
2025-02-16 03:39:59 +0800

    installed, ensure

--- common.zsh
+++ common.zsh
@@ -3,10 +3,14 @@ NOT_YET_INSTALL=""
 
 
 # check if binary exists in PATH
-# to be used as a guarding statement, put at first line of a script file: 
+installed () {
+    command -v "$1" 2>&1 >/dev/null
+}
+
+# helper to be used as a guarding statement, put at first line of a script file: 
 #   ensure <binary name> || return
 ensure () {
-    if command -v "$1" 2>&1 >/dev/null; then
+    if installed $1; then
         return 0
     else
         NOT_YET_INSTALL="$NOT_YET_INSTALL $1"
