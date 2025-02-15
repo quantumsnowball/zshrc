6f8f740f9f46eef36a3942294ece29cd0e5924cc
Quantum Snowball <quantum.snowball@gmail.com>
2025-02-16 03:21:12 +0800

    wtf-to-install()

--- common.zsh
+++ common.zsh
@@ -1,6 +1,22 @@
+# global vars
+NOT_YET_INSTALL=""
+
+
 # check if binary exists in PATH
 # to be used as a guarding statement, put at first line of a script file: 
 #   ensure <binary name> || return
 ensure () {
-    command -v "$1" 2>&1 >/dev/null
+    if command -v "$1" 2>&1 >/dev/null; then
+        return 0
+    else
+        NOT_YET_INSTALL="$NOT_YET_INSTALL $1"
+        return 1
+    fi
 }
+
+
+# suggest user what to install next
+wtf-to-install () {
+    echo $NOT_YET_INSTALL | xargs
+}
+
