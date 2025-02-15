1934f41b9347c455124ad430b2b29a04e32397ab
Quantum Snowball <quantum.snowball@gmail.com>
2025-02-16 03:52:02 +0800

    jupyter

--- utils/jupyter.zsh
+++ utils/jupyter.zsh
@@ -1,3 +1,6 @@
+ensure jupyter || return
+
+
 # wsl2 may not be able to launch the default browser
 # causing jupyter server to stuck in the launch
 # start jupyter server with '--no-browser' flag will dirty fix this problem
