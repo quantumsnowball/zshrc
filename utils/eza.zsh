dc9be58e321170dcb2bd0f29136ad159202e00c6
Quantum Snowball <quantum.snowball@gmail.com>
2025-02-15 20:40:15 +0800

    better ll

--- utils/eza.zsh
+++ utils/eza.zsh
@@ -3,6 +3,7 @@ alias ls='eza -F --icons'
 alias  l='eza -F --icons'
 alias la='eza -aF --icons'
 alias lt='eza -aF -s time --reverse --icons'
-alias ll='eza -aF -l -h --icons --grid'
+alias ll='eza -aF -l -h --icons'
+alias lll='eza -aF -l -h --icons --grid'
 alias lT='eza -aF -l -h --icons --tree --level=2'
 
