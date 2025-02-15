85f4d01f870233966aafcf16dd3e0e026a88fc65
Quantum Snowball <quantum.snowball@gmail.com>
2025-02-16 03:40:02 +0800

    apt

--- utils/apt.zsh
+++ utils/apt.zsh
@@ -1,12 +1,13 @@
+installed apt || return
+
+
 # apt
-if command -v apt &> /dev/null; then 
-    alias apti='sudo apt install'
-    alias apts='apt search'
-    alias aptls='apt list'
-    alias aptgrep='apt list | rg'
-    alias aptrg=aptgrep
-    alias aptrm='sudo apt remove'
-    alias aptu='sudo apt update'
-    alias aptup='sudo apt update && sudo apt upgrade'
-fi
+alias apti='sudo apt install'
+alias apts='apt search'
+alias aptls='apt list'
+alias aptgrep='apt list | rg'
+alias aptrg=aptgrep
+alias aptrm='sudo apt remove'
+alias aptu='sudo apt update'
+alias aptup='sudo apt update && sudo apt upgrade'
 
