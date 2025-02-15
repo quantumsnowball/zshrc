aaac8037975126eb0cf3acdcea5c5e828408af71
Quantum Snowball <quantum.snowball@gmail.com>
2025-02-16 01:42:57 +0800

    condi alias for docker

--- utils/docker.zsh
+++ utils/docker.zsh
@@ -1,9 +1,9 @@
 # docker
-alias dc="docker-compose"
-alias dk="docker"
-alias dkr="docker run -it"
-alias dks="docker start"
-alias dka="docker attach"
-alias dkps="docker ps -a | most"
-
-
+if command -v docker &> /dev/null; then
+    alias dc="docker-compose"
+    alias dk="docker"
+    alias dkr="docker run -it"
+    alias dks="docker start"
+    alias dka="docker attach"
+    alias dkps="docker ps -a | most"
+fi
