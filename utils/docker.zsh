dad320231aa8f268c2e9dda0e6c50e9caff598e6
Quantum Snowball <quantum.snowball@gmail.com>
2025-02-16 03:29:04 +0800

    docker

--- utils/docker.zsh
+++ utils/docker.zsh
@@ -1,9 +1,10 @@
+ensure docker || return
+
+
 # docker
-if command -v docker &> /dev/null; then
-    alias dc="docker-compose"
-    alias dk="docker"
-    alias dkr="docker run -it"
-    alias dks="docker start"
-    alias dka="docker attach"
-    alias dkps="docker ps -a | most"
-fi
+alias dc="docker-compose"
+alias dk="docker"
+alias dkr="docker run -it"
+alias dks="docker start"
+alias dka="docker attach"
+alias dkps="docker ps -a | most"
