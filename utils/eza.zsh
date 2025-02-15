b411fe67dd77f70a5792aa722cd05f787828fde3
Quantum Snowball <quantum.snowball@gmail.com>
2025-02-16 03:27:49 +0800

    eza

--- utils/eza.zsh
+++ utils/eza.zsh
@@ -1,10 +1,11 @@
+ensure eza || return
+
+
 # eza as ls
-if command -v eza &> /dev/null; then
-    alias ls='eza -F --icons'
-    alias  l='eza -F --icons'
-    alias la='eza -aF --icons'
-    alias lt='eza -aF -s time --reverse --icons'
-    alias ll='eza -aF -l -h --icons'
-    alias lll='eza -aF -l -h --icons --grid'
-    alias lT='eza -aF -l -h --icons --tree --level=2'
-fi
+alias ls='eza -F --icons'
+alias  l='eza -F --icons'
+alias la='eza -aF --icons'
+alias lt='eza -aF -s time --reverse --icons'
+alias ll='eza -aF -l -h --icons'
+alias lll='eza -aF -l -h --icons --grid'
+alias lT='eza -aF -l -h --icons --tree --level=2'
