a41984e7cfb269612d5266729e10899abd2d0bfb
Quantum Snowball <quantum.snowball@gmail.com>
2025-02-18 19:12:13 +0800

    some ssh helpers

--- init/ssh.zsh
+++ init/ssh.zsh
@@ -1,6 +1,13 @@
 ensure ssh || return
 
 
+# helpers
+alias ssh.my-authorized-keys='cat ~/.ssh/authorized_keys'
+alias ssh.my-config='cat ~/.ssh/config'
+alias ssh.my-public-key='cat ~/.ssh/*.pub'
+alias ssh.reset-known-hosts='rm ~/.ssh/known_hosts'
+
+
 # omz ssh-agent plugin should already started a ssh-agent process
 # it should ask for password the first time
 
