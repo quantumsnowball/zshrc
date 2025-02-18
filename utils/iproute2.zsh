c3d49ce73fc8842ddcd4e9cdefe5379cb4c06694
Quantum Snowball <quantum.snowball@gmail.com>
2025-02-19 03:17:19 +0800

    iproute2, ip.local namespace

--- utils/iproute2.zsh
+++ utils/iproute2.zsh
@@ -1,15 +1,20 @@
-    # list links, ip addresses, routes
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    lslink() { ip link; }
-    lsip() { ip addr; }
-    lsip4() { ip -4 addr;}
-    lsip6() { ip -6 addr;}
-    lsroute() { ip route; }
-else
-    alias ip='ip -c'
-    lslink() { ip -c -br link; }
-    lsip() { ip -c -br addr; }
-    lsip4() { ip -c -br -4 addr;}
-    lsip6() { ip -c -br -6 addr;}
-    lsroute() { ip -c route; }
-fi
+ensure ip || return
+
+
+[[ "$OSTYPE" == "darwin"* ]] || alias ip='ip -c'
+
+
+ip.local.ls-link () { ip -c -br link; }
+alias lslink=ip.local.ls-link
+
+ip.local.ls-addr () { ip -c -br addr; }
+alias lsip=ip.local.ls-addr
+
+ip.local.ls-addr4 () { ip -c -br -4 addr;}
+alias lsip4=ip.local.ls-addr4
+
+ip.local.ls-addr6 () { ip -c -br -6 addr;}
+alias lsip6=ip.local.ls-addr6
+
+ip.local.routing-table () { ip -c route; }
+alias lsroute=ip.local.routing-table
