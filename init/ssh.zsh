aaf73ddd98ec5f5a37bb009e7ed456fef0370155
Quantum Snowball <quantum.snowball@gmail.com>
2025-02-19 03:53:21 +0800

    ssh helpers and ssh namespace should be located at utils/

--- init/ssh.zsh
+++ init/ssh.zsh
@@ -1,56 +1,8 @@
 ensure ssh || return
 
 
-# helpers
-alias ssh.my-authorized-keys='cat ~/.ssh/authorized_keys'
-alias ssh.my-config='cat ~/.ssh/config'
-alias ssh.my-public-key='cat ~/.ssh/*.pub'
-alias ssh.reset-known-hosts='rm ~/.ssh/known_hosts'
-ssh.touch-remote () {
-    [ -f ~/.config/ssh/touch-remote ] || return 1
-    . ~/.config/ssh/touch-remote $1
-}
-ssh.install-locally () {
-    # must ensure downloaded the repo first
-    [ -f ~/.config/ssh/install ] || {(
-        mkdir -p ~/.config && cd ~/.config
-        gh.clone-my-repo -token ssh
-    )}
-    # this create the necessary symlinks
-    [ -f ~/.config/ssh/install ] && . ~/.config/ssh/install
-}
-ssh.uninstall-locally () {
-    # uninstall the symlinks and restore a real file
-    cp ~/.config/ssh/authorized_keys ~/.ssh/authorized_keys.real &&
-    mv ~/.ssh/authorized_keys.real ~/.ssh/authorized_keys
-
-    cp ~/.config/ssh/config ~/.ssh/config.real &&
-    mv ~/.ssh/config.real ~/.ssh/config
-}
-ssh.status () {
-    local dst=~/.ssh/authorized_keys
-    [ -f $dst ] || { 
-        echo "\n[ FAILED ]\nFile $dst does not exists.\nHost may not be reachable via SSH.\n"
-        return 1
-    }
-    [ -L ~/.ssh/authorized_keys ] || {
-        echo "\n[ FAILED ]\nFile $dst is static.\nIt may need to manually updated to enable more hosts.\n"
-        return 1
-    }
-    local src=$(readlink -f $dst)
-    local correct_src="$HOME/.config/ssh/authorized_keys"
-    [ $src = $correct_src ] || {
-        echo "\n[ FAILED ]\nFile $dst is wrongly linked to $src.\nRun ssh.install-locally again may fix it.\n"
-        return 1
-    }
-    echo "\n[ SUCCESS ]\nFile $dst is linked correctly to $correct_src.\n"
-    return 0    
-}
-
-
 # omz ssh-agent plugin should already started a ssh-agent process
 # it should ask for password the first time
-
 
 #
 # put ssh-agent settings here
@@ -64,6 +16,5 @@ else
   SHORT_HOST="${HOST/.*/}"
 fi
 
-
-# sshd
+# ensure sshd is running on Termux
 [ -v TERMUX_VERSION ] && sshd
---
+++ utils/ssh.zsh
@@ -0,0 +1,50 @@
+ensure ssh || return
+
+
+# helpers
+alias ssh.my-authorized-keys='cat ~/.ssh/authorized_keys'
+alias ssh.my-config='cat ~/.ssh/config'
+alias ssh.my-public-key='cat ~/.ssh/*.pub'
+alias ssh.reset-known-hosts='rm ~/.ssh/known_hosts'
+ssh.touch-remote () {
+    [ -f ~/.config/ssh/touch-remote ] || return 1
+    . ~/.config/ssh/touch-remote $1
+}
+ssh.install-locally () {
+    # must ensure downloaded the repo first
+    [ -f ~/.config/ssh/install ] || {(
+        mkdir -p ~/.config && cd ~/.config
+        gh.clone-my-repo -token ssh
+    )}
+    # this create the necessary symlinks
+    [ -f ~/.config/ssh/install ] && . ~/.config/ssh/install
+}
+ssh.uninstall-locally () {
+    # uninstall the symlinks and restore a real file
+    cp ~/.config/ssh/authorized_keys ~/.ssh/authorized_keys.real &&
+    mv ~/.ssh/authorized_keys.real ~/.ssh/authorized_keys
+
+    cp ~/.config/ssh/config ~/.ssh/config.real &&
+    mv ~/.ssh/config.real ~/.ssh/config
+}
+ssh.status () {
+    local dst=~/.ssh/authorized_keys
+    [ -f $dst ] || { 
+        echo "\n[ FAILED ]\nFile $dst does not exists.\nHost may not be reachable via SSH.\n"
+        return 1
+    }
+    [ -L ~/.ssh/authorized_keys ] || {
+        echo "\n[ FAILED ]\nFile $dst is static.\nIt may need to manually updated to enable more hosts.\n"
+        return 1
+    }
+    local src=$(readlink -f $dst)
+    local correct_src="$HOME/.config/ssh/authorized_keys"
+    [ $src = $correct_src ] || {
+        echo "\n[ FAILED ]\nFile $dst is wrongly linked to $src.\nRun ssh.install-locally again may fix it.\n"
+        return 1
+    }
+    echo "\n[ SUCCESS ]\nFile $dst is linked correctly to $correct_src.\n"
+    return 0    
+}
+
+
