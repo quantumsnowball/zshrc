0801c90438a1deb4f4b31fd05b233908d2e3eae5
Quantum Snowball <quantum.snowball@gmail.com>
2025-02-19 23:57:22 +0800

    show specific chain

--- utils/iptables.zsh
+++ utils/iptables.zsh
@@ -3,11 +3,21 @@ ensure iptables || return
 
 # Written and tested on CentOS 7
 ip.tables = () {
-    sudo iptables -vL | 
+    sudo iptables -vL --line-numbers | 
         cut -f -9 | 
         column -t | 
         sed 's/^Chain/\n&/g' | 
         sed '/^Chain/ s/[ \t]\{1,\}/ /g' | 
         sed '/^[0-9]/ s/[ \t]\{1,\}/ /10g' | 
         bat --wrap=never --style=grid --color=always
+}
+
+ip.tables.input = () {
+    sudo iptables -vL INPUT --line-numbers
+}
+ip.tables.forward = () {
+    sudo iptables -vL FORWARD --line-numbers
+}
+ip.tables.output = () {
+    sudo iptables -vL OUTPUT --line-numbers
 }
