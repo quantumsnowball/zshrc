db073870a7dec437b8991f45ed05f3641620a630
Quantum Snowball <quantum.snowball@gmail.com>
2025-02-19 16:13:34 +0800

    wtf.in()

--- utils/help.zsh
+++ utils/help.zsh
@@ -1,5 +1,5 @@
-wtf()
-{
+# a basic helper to ask what a command is
+wtf() {
     echo -e "\n>_ ${GREEN}command -v${RESET} ${BLUE}$1${RESET}"
     command -v $1
 
@@ -11,3 +11,16 @@ wtf()
 
     echo -e "\n"
 }
+
+
+# a more detailed version of wtf which can also show the content of a function
+wtf.in() {
+    echo -e "\n>_ ${GREEN}type -a${RESET} ${BLUE}$1${RESET}"
+    type -a $1
+
+    installed which || return
+
+    echo -e "\n>_ ${GREEN}which${RESET} ${BLUE}$1${RESET}"
+    which $1
+}
+alias wtfin='wtf.in'
