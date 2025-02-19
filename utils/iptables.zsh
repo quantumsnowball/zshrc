50dc81c3438e780bdcd64bbd07ba803f0143ecd6
Quantum Snowball <quantum.snowball@gmail.com>
2025-02-20 01:59:27 +0800

    delete-line

--- utils/iptables.zsh
+++ utils/iptables.zsh
@@ -5,22 +5,33 @@ ensure iptables || return
 ip.tables = () {
     sudo iptables -vL --line-numbers | less -c -S -F
 }
-
 ip.tables.input = () {
     sudo iptables -vL INPUT --line-numbers | less -c -S -F
 }
-ip.tables.input.delete-line () {
-    ip.tables.input
-    echo -n "\nLine number to be deleted: " ; read line ; echo ""
-    sudo iptables -D INPUT $line && 
-        echo "\nResult: ${GREEN}SUCCESS${RESET}\n" || 
-        echo "\nResult: ${RED}FAILED${RESET}\n"
-}
-
 ip.tables.forward = () {
     sudo iptables -vL FORWARD --line-numbers | less -c -S -F
 }
-
 ip.tables.output = () {
     sudo iptables -vL OUTPUT --line-numbers | less -c -S -F
+}
+
+# DELETE
+ip.tables.delete-filter () {
+    echo "${YELLOW}Current filter rules:${RESET}"
+    sudo iptables -L INPUT --line-numbers
+    sudo iptables -L FORWARD --line-numbers
+    sudo iptables -L OUTPUT --line-numbers
+    echo -n "${CYAN}Select a table [INPUT/FORWARD/OUTPUT]: ${RESET}" ; read name
+    echo -n "${CYAN}Line number to be deleted: ${RESET}" ; read line ; echo ""
+    sudo iptables -D $name $line && 
+        echo "\nResult: ${GREEN}SUCCESS${RESET}\n" || 
+        echo "\nResult: ${RED}FAILED${RESET}\n"
+}
+ip.tables.input.delete-filter () {
+    echo "${YELLOW}Current filter rules:${RESET}"
+    sudo iptables -L INPUT --line-numbers
+    echo -n "${CYAN}Line number to be deleted: ${RESET}" ; read line ; echo ""
+    sudo iptables -D INPUT $line && 
+        echo "\nResult: ${GREEN}SUCCESS${RESET}\n" || 
+        echo "\nResult: ${RED}FAILED${RESET}\n"
 }
