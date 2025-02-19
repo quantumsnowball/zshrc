130ea6d0fd67f04af8fc5d7409df9b9f50d229dc
Quantum Snowball <quantum.snowball@gmail.com>
2025-02-20 02:35:04 +0800

    ip.tables.input.open-ports

--- utils/iptables.zsh
+++ utils/iptables.zsh
@@ -14,13 +14,39 @@ ip.tables.forward = () {
 ip.tables.output = () {
     sudo iptables -vL OUTPUT --line-numbers | less -c -S -F
 }
+__ip_tables_current () {
+    echo "${YELLOW}Current filter rules:${RESET}"
+    sudo iptables -L INPUT --line-numbers ; echo
+    sudo iptables -L FORWARD --line-numbers ; echo
+    sudo iptables -L OUTPUT --line-numbers ; echo
+}
+__ip_tables_input_current () {
+    echo "${YELLOW}Current filter rules:${RESET}"
+    sudo iptables -L INPUT --line-numbers ; echo
+}
+
+
+# ADD
+__ip_tables_input_open_ports () {
+    sudo iptables -I INPUT 1 -p $1 --dport $2 -j ACCEPT &&
+        echo "\nResult: ${GREEN}SUCCESS${RESET}\n" || 
+        echo "\nResult: ${RED}FAILED${RESET}\n"
+}
+ip.tables.input.open-tcp-ports () {
+    __ip_tables_input_current
+    echo -n "${CYAN}TCP port or range of ports to allow: ${RESET}" ; read dport ; echo ""
+    __ip_tables_input_open_ports tcp $dport
+}
+ip.tables.input.open-udp-ports () {
+    __ip_tables_input_current
+    echo -n "${CYAN}UDP port or range of ports to allow: ${RESET}" ; read dport ; echo ""
+    __ip_tables_input_open_ports udp $dport
+}
+
 
 # DELETE
 ip.tables.delete-filter () {
-    echo "${YELLOW}Current filter rules:${RESET}"
-    sudo iptables -L INPUT --line-numbers
-    sudo iptables -L FORWARD --line-numbers
-    sudo iptables -L OUTPUT --line-numbers
+    __ip_tables_current
     echo -n "${CYAN}Select a table [INPUT/FORWARD/OUTPUT]: ${RESET}" ; read name
     echo -n "${CYAN}Line number to be deleted: ${RESET}" ; read line ; echo ""
     sudo iptables -D $name $line && 
@@ -28,8 +54,7 @@ ip.tables.delete-filter () {
         echo "\nResult: ${RED}FAILED${RESET}\n"
 }
 ip.tables.input.delete-filter () {
-    echo "${YELLOW}Current filter rules:${RESET}"
-    sudo iptables -L INPUT --line-numbers
+    __ip_tables_input_current
     echo -n "${CYAN}Line number to be deleted: ${RESET}" ; read line ; echo ""
     sudo iptables -D INPUT $line && 
         echo "\nResult: ${GREEN}SUCCESS${RESET}\n" || 
