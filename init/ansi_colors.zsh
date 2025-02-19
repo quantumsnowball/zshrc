563d8d29fc613a7938f0d79112cd0433dabe45ad
Quantum Snowball <quantum.snowball@gmail.com>
2025-02-19 15:35:10 +0800

    added some preset ansi color for zsh

---
+++ init/ansi_colors.zsh
@@ -0,0 +1,10 @@
+# Define colors
+BLACK="\033[0;30m"    # black
+RED="\033[0;31m"      # red
+GREEN="\033[0;32m"    # green
+YELLOW="\033[0;33m"   # yellow
+BLUE="\033[0;34m"     # blue
+MAGENTA="\033[0;35m"  # magenta
+CYAN="\033[0;36m"     # cyan
+WHITE="\033[0;37m"    # white
+RESET="\033[0m"       #
