d6238585fb0eb97072919e47d8bfeac2d0afbf49
Quantum Snowball <quantum.snowball@gmail.com>
2025-02-21 05:18:00 +0800

    ps namespace

--- utils/process.zsh
+++ utils/process.zsh
@@ -1,4 +1,7 @@
 # process tree info
-alias lsps='ps -eHo pid,comm'
-alias lsps2='pstree -p | bat'
-alias findps='lsps | rg -B2 -A1'
+alias ps.process='ps -eHo pid,comm'
+alias lsps=ps.process
+alias ps.process-tree='pstree -p | bat'
+alias lsps2=ps.process-tree
+alias ps.highlight='lsps | rg -B2 -A1'
+alias findps=ps.highlight
