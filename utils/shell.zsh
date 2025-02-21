679fdd42feb44168e29937183ee6fe33340bbcc9
Quantum Snowball <quantum.snowball@gmail.com>
2025-02-22 00:36:05 +0800

    reload and resourcing, zsh namespace

--- utils/shell.zsh
+++ utils/shell.zsh
@@ -9,8 +9,16 @@ alias root='/bin/sudo -E -s'
 alias c=clear
 alias cl=clear
 # reload shell
-alias rr='source ~/.zshrc'
-alias rrr='exec zsh'
+zsh.reload-shell () {
+    export ZSH_RELOADING=true
+    exec zsh
+}
+alias rr=zsh.reload-shell
+zsh.resource-zshrc () {
+    export ZSH_RESOURCING=true
+    source ~/.zshrc
+}
+alias rrr=zsh.resource-zshrc
 # exit
 alias x=exit
 # process
