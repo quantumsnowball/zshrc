52e30a003487d23ea5c6e8b9a975002adcfa2ca8
Quantum Snowball <quantum.snowball@gmail.com>
2025-02-22 21:44:04 +0800

    comment

--- init/ssh-agent.zsh
+++ init/ssh-agent.zsh
@@ -7,25 +7,30 @@
 ssh_env_cache="$HOME/.ssh/environment-$SHORT_HOST"
 
 function ssh-agent.start() {
-  # Check if ssh-agent is already running
+  # ensure environment present
   if [[ -f "$ssh_env_cache" ]]; then
     . "$ssh_env_cache" > /dev/null
 
-    # Test if $SSH_AUTH_SOCK is visible
+    # if socket is also visible then ssh-agent is ready, nicely exit
     zmodload zsh/net/socket
     if [[ -S "$SSH_AUTH_SOCK" ]] && zsocket "$SSH_AUTH_SOCK" 2>/dev/null; then
       return 0
     fi
   fi
 
+  # if ssh-agent not ready, proceed to recreate it
+
+  # first ensure ~/.ssh/ directory
   if [[ ! -d "$HOME/.ssh" ]]; then
-    echo "[oh-my-zsh] ssh-agent plugin requires ~/.ssh directory"
+    echo "ssh-agent requires ~/.ssh directory"
     return 1
   fi
 
-  # start ssh-agent and setup environment
+  # start ssh-agent ps and cache the setup environment script
   echo "Starting ssh-agent ..."
   ssh-agent -s | sed '/^echo/d' >! "$ssh_env_cache"
+
+  # execute the script to create the environment
   chmod 600 "$ssh_env_cache"
   . "$ssh_env_cache" > /dev/null
 }
@@ -37,7 +42,8 @@ function ssh-agent.add-keys() {
 
   # check for .ssh folder presence
   if [[ ! -d "$HOME/.ssh" ]]; then
-    return
+    echo "ssh-agent requires ~/.ssh directory"
+    return 1
   fi
 
   # add default keys if no identities were set up via zstyle
