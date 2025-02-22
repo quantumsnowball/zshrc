5bc9364f28197a8877dfecf58f0caae7a1da71c8
Quantum Snowball <quantum.snowball@gmail.com>
2025-02-22 21:51:41 +0800

    don't need these garbage, actually just need to run ssh-add

--- init/ssh-agent.zsh
+++ init/ssh-agent.zsh
@@ -36,71 +36,14 @@ function ssh-agent.start() {
 }
 
 function ssh-agent.add-keys() {
-  local id file line sig lines
-  local -a identities loaded_sigs loaded_ids not_loaded
-  zstyle -a :omz:plugins:ssh-agent identities identities
-
   # check for .ssh folder presence
   if [[ ! -d "$HOME/.ssh" ]]; then
     echo "ssh-agent requires ~/.ssh directory"
     return 1
   fi
 
-  # add default keys if no identities were set up via zstyle
-  # this is to mimic the call to ssh-add with no identities
-  if [[ ${#identities} -eq 0 ]]; then
-    # key list found on `ssh-add` man page's DESCRIPTION section
-    for id in id_rsa id_dsa id_ecdsa id_ed25519 id_ed25519_sk identity; do
-      # check if file exists
-      [[ -f "$HOME/.ssh/$id" ]] && identities+=($id)
-    done
-  fi
-
-  # get list of loaded identities' signatures and filenames
-  if lines=$(ssh-add -l); then
-    for line in ${(f)lines}; do
-      loaded_sigs+=${${(z)line}[2]}
-      loaded_ids+=${${(z)line}[3]}
-    done
-  fi
-
-  # add identities if not already loaded
-  for id in $identities; do
-    # if id is an absolute path, make file equal to id
-    [[ "$id" = /* ]] && file="$id" || file="$HOME/.ssh/$id"
-    # check for filename match, otherwise try for signature match
-    if [[ -f $file && ${loaded_ids[(I)$file]} -le 0 ]]; then
-      sig="$(ssh-keygen -lf "$file" | awk '{print $2}')"
-      [[ ${loaded_sigs[(I)$sig]} -le 0 ]] && not_loaded+=("$file")
-    fi
-  done
-
-  # abort if no identities need to be loaded
-  if [[ ${#not_loaded} -eq 0 ]]; then
-    return
-  fi
-
-  # pass extra arguments to ssh-add
-  local args
-  zstyle -a :omz:plugins:ssh-agent ssh-add-args args
-
-  # if ssh-agent quiet mode, pass -q to ssh-add
-  zstyle -t :omz:plugins:ssh-agent quiet && args=(-q $args)
-
-  # use user specified helper to ask for password (ksshaskpass, etc)
-  local helper
-  zstyle -s :omz:plugins:ssh-agent helper helper
-
-  if [[ -n "$helper" ]]; then
-    if [[ -z "${commands[$helper]}" ]]; then
-      echo >&2 "ssh-agent: the helper '$helper' has not been found."
-    else
-      SSH_ASKPASS="$helper" ssh-add "${args[@]}" ${^not_loaded} < /dev/null
-      return $?
-    fi
-  fi
-
-  ssh-add "${args[@]}" ${^not_loaded}
+  # this will auto discover and add keys
+  ssh-add
 }
 
 # init
