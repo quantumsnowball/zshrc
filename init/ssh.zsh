ad1c532c426af672ecd145f2082d6561f9a77f01
Quantum Snowball <quantum.snowball@gmail.com>
2025-02-22 23:14:39 +0800

    patched ssh

--- init/ssh.zsh
+++ init/ssh.zsh
@@ -1,5 +1,14 @@
 ensure ssh || return
 
 
+# patched command to ensure discovery of ssh-agent
+ssh () {
+    # try to load ssh-agent setup-env script
+    [ -f ~/.ssh/.env ] && . ~/.ssh/.env
+    # execute the actual command
+    command ssh "$@"
+}
+
+
 # ensure sshd is running on Termux
 [ -v TERMUX_VERSION ] && sshd
