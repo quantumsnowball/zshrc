518b6025c2044b07c56287b68f6517b64170ec2c
Quantum Snowball <quantum.snowball@gmail.com>
2025-02-22 23:10:50 +0800

    don't even need this cause we don't use omz ssh-agent plugin

--- init/ssh.zsh
+++ init/ssh.zsh
@@ -1,20 +1,5 @@
 ensure ssh || return
 
 
-# omz ssh-agent plugin should already started a ssh-agent process
-# it should ask for password the first time
-
-#
-# put ssh-agent settings here
-#
-
-# Figure out the SHORT hostname
-if [[ "$OSTYPE" = darwin* ]]; then
-  # macOS's $HOST changes with dhcp, etc. Use ComputerName if possible.
-  SHORT_HOST=$(scutil --get ComputerName 2>/dev/null) || SHORT_HOST="${HOST/.*/}"
-else
-  SHORT_HOST="${HOST/.*/}"
-fi
-
 # ensure sshd is running on Termux
 [ -v TERMUX_VERSION ] && sshd
