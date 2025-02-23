b74182f78d4ab25b6b22759bb91a6a5ce418bf2b
Quantum Snowball <quantum.snowball@gmail.com>
2025-02-23 16:04:25 +0800

    keychain.start, kc

--- utils/keychain.zsh
+++ utils/keychain.zsh
@@ -1,3 +1,11 @@
 ensure keychain || return
 
 
+keychain.start () {
+    # if ssh-agent is not running, start it
+    # if ssh-agent is already running, find it
+    # anyway, will output the necessary env and eval it
+    eval $(keychain --eval)
+}
+alias kc=keychain.start
+
