3377f0c0172ca0559fc91d3c9d308ab4c24e212a
Quantum Snowball <quantum.snowball@gmail.com>
2025-02-23 16:48:54 +0800

    kc for the most used alias

--- utils/keychain.zsh
+++ utils/keychain.zsh
@@ -1,22 +1,26 @@
 ensure keychain || return
 
 
-keychain.start () {
+kc.start () {
     # if ssh-agent is not running, start it
     # if ssh-agent is already running, find it
     # anyway, will output the necessary env and eval it
     # can accept any keychain args
     eval $(keychain --eval $@)
 }
-alias kc.start=keychain.start
 
-keychain.add () {
+# this is the most common usecase, has to be short and handy
+# call this in a new shell, or the current shell, or inside of lazygiit 
+# whereever necessary to add a key it should let most other shell instance 
+# able to find the added key
+kc.add () {
     # normally boot up keychain
     eval $(keychain --eval --quiet)
     # then add default keys
     ssh-add
 }
-alias kc=keychain.add
+alias kc=kc.add
+
 
 # start kc by default
 # - start quietly ssh-agent quietly if not alreay running
