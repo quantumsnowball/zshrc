2ed42a18df5a8000fdd161293bac812f61c1f629
Quantum Snowball <quantum.snowball@gmail.com>
2025-02-23 16:21:09 +0800

    accept args, more alias

--- utils/keychain.zsh
+++ utils/keychain.zsh
@@ -5,7 +5,9 @@ keychain.start () {
     # if ssh-agent is not running, start it
     # if ssh-agent is already running, find it
     # anyway, will output the necessary env and eval it
-    eval $(keychain --eval)
+    # can accept any keychain args
+    eval $(keychain --eval $@)
 }
-alias kc=keychain.start
+alias kc.start=keychain.start
+alias kc='keychain.start --quiet'
 
