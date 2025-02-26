b380f87437eeea039529c6769c16e5a1c5fad349
Quantum Snowball <quantum.snowball@gmail.com>
2025-02-27 01:36:10 +0800

    prefix and suffix to profile zsh

--- __init__.zsh
+++ __init__.zsh
@@ -1,3 +1,5 @@
+# zmodload zsh/zprof
+
 source $HOME/.config/zshrc/common.zsh
 source $HOME/.config/zshrc/paths/__init__.zsh
 source $HOME/.config/zshrc/init/__init__.zsh
@@ -6,3 +8,5 @@ source $HOME/.config/zshrc/utils/__init__.zsh
 
 [[ $ZSH_RELOADING == true ]] && echo "INFO: zsh reloaded" && unset ZSH_RELOADING || true
 [[ $ZSH_RESOURCING == true ]] && echo "INFO: ~/.zshrc reloaded" && unset ZSH_RESOURCING || true
+
+# zprof
