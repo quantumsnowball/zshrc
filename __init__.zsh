6259d86a2cee6f392e74644efdf37f0ce3f4cb14
Quantum Snowball <quantum.snowball@gmail.com>
2025-02-27 00:44:04 +0800

    fix p10k instant prompt warning

--- __init__.zsh
+++ __init__.zsh
@@ -5,6 +5,7 @@ source $HOME/.config/zshrc/paths/__init__.zsh
 source $HOME/.config/zshrc/init/__init__.zsh
 source $HOME/.config/zshrc/plugins/__init__.zsh
 source $HOME/.config/zshrc/utils/__init__.zsh
+source $HOME/.config/zshrc/theme/p10k.zsh
 
 [[ $ZSH_RELOADING == true ]] && echo "INFO: zsh reloaded" && unset ZSH_RELOADING || true
 [[ $ZSH_RESOURCING == true ]] && echo "INFO: ~/.zshrc reloaded" && unset ZSH_RESOURCING || true
