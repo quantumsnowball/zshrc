ca535f0da6773a10e6674809de524523efcf48c9
Quantum Snowball <quantum.snowball@gmail.com>
2025-03-03 18:44:41 +0800

    inside menu press esc or q to send-break

--- init/autocomplete.zsh
+++ init/autocomplete.zsh
@@ -22,6 +22,8 @@ bindkey -M menuselect 'h' vi-backward-char
 bindkey -M menuselect 'k' vi-up-line-or-history
 bindkey -M menuselect 'j' vi-down-line-or-history
 bindkey -M menuselect 'l' vi-forward-char
+bindkey -M menuselect '\e' send-break
+bindkey -M menuselect 'q' send-break
 
 # details
 zstyle ':completion:*' complete-options true
