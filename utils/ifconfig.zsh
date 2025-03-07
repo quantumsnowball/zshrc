d97e4bf342bec06b35f63f4af8cc7ddeb35ce55d
Quantum Snowball <quantum.snowball@gmail.com>
2025-03-07 18:57:59 +0800

    better coloring

--- utils/ifconfig.zsh
+++ utils/ifconfig.zsh
@@ -1,6 +1,6 @@
 ifconfig.local.addr () {
-    ifconfig | awk '
-    /^[a-zA-Z0-9]/ { iface=$1 }
-    /inet / { print iface, $2, $4 }
+    ifconfig | awk -v cyan="$CYAN" -v green="$GREEN" -v red="$RED" -v reset="$RESET" '
+    /^[a-zA-Z0-9]/ { iface=$1; sub(/:/, "", iface) }
+    /inet / { print cyan iface reset, green $2 reset, red $4 reset }
     ' | column -t
 }
