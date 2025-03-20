4d3ff530a211a357ee184b60be42daa2264892a7
Quantum Snowball <quantum.snowball@gmail.com>
2025-03-20 23:31:11 +0800

    default to enable color

--- utils/iproute2.zsh
+++ utils/iproute2.zsh
@@ -1,6 +1,8 @@
 ensure ip || return
 
 
+alias ip='ip -c'
+
 _ip-addr-pretty-print() {
     awk '{
         printf "%-22s%s", $1, $2;  # Print interface and state
@@ -14,31 +16,31 @@ _ip-addr-pretty-print() {
 }
 
 ip.local.link () { 
-    ip -c -br link; 
+    ip -br link; 
 }
 alias lslink=ip.local.link
 alias lsnic=ip.local.link
 
 ip.local.addr () {
-    ip -c -br addr | _ip-addr-pretty-print || ifconfig.local.addr
+    ip -br addr | _ip-addr-pretty-print || ifconfig.local.addr
 }
 alias lsip=ip.local.addr
 alias myip=ip.local.addr
 
 ip.local.addr4 () {
-    ip -c -br -4 addr | _ip-addr-pretty-print || ifconfig.local.addr
+    ip -br -4 addr | _ip-addr-pretty-print || ifconfig.local.addr
 }
 alias lsip4=ip.local.addr4
 alias myip4=ip.local.addr4
 
 ip.local.addr6 () {
-    ip -c -br -6 addr | _ip-addr-pretty-print
+    ip -br -6 addr | _ip-addr-pretty-print
 }
 alias lsip6=ip.local.addr6
 alias myip6=ip.local.addr6
 
 ip.routing-table () {
-    ip -c route; 
+    ip route; 
 }
 alias lsroute=ip.routing-table
 alias ip.route=ip.routing-table
