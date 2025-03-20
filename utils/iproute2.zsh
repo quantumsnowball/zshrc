3fd58c82b09a30b3653dcf06a69431d3440516e9
Quantum Snowball <quantum.snowball@gmail.com>
2025-03-20 22:58:41 +0800

    ip addr pp

--- utils/iproute2.zsh
+++ utils/iproute2.zsh
@@ -1,6 +1,18 @@
 ensure ip || return
 
 
+_ip-addr-pretty-print() {
+    awk '{
+        printf "%-22s%s", $1, $2;  # Print interface and state
+        if (NF > 2) {
+            printf "\n"
+            for (i = 3; i < NF; i++) {
+                printf "    %s\n", $i;  # Indent each address with 4 spaces
+            }
+        }
+    }'
+}
+
 ip.local.link () { 
     ip -c -br link; 
 }
@@ -8,19 +20,19 @@ alias lslink=ip.local.link
 alias lsnic=ip.local.link
 
 ip.local.addr () {
-    ip -c -br addr || ifconfig.local.addr
+    ip -c -br addr | _ip-addr-pretty-print || ifconfig.local.addr
 }
 alias lsip=ip.local.addr
 alias myip=ip.local.addr
 
 ip.local.addr4 () {
-    ip -c -br -4 addr || ifconfig.local.addr
+    ip -c -br -4 addr | _ip-addr-pretty-print || ifconfig.local.addr
 }
 alias lsip4=ip.local.addr4
 alias myip4=ip.local.addr4
 
 ip.local.addr6 () {
-    ip -c -br -6 addr
+    ip -c -br -6 addr | _ip-addr-pretty-print
 }
 alias lsip6=ip.local.addr6
 alias myip6=ip.local.addr6
