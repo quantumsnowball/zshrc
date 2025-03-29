2414788b8687b49183c0225bada9285fa4cf686b
Quantum Snowball <quantum.snowball@gmail.com>
2025-03-29 22:03:06 +0800

    nvim.lazy.rm-all-plugins

--- utils/nvim.zsh
+++ utils/nvim.zsh
@@ -5,3 +5,9 @@ ensure nvim || return
 alias vi='nvim'
 alias v='nvim'
 
+nvim.lazy.rm-all-plugins () {
+    $lazy_dir=~/.local/share/nvim/lazy/
+    read -q "confirm?Are you sure you want to remove all lazy plugins? (y/n): "
+    [[ "$confirm" =~ ^[Yy]$ ]] && rm -rf $lazy_dir
+    [[ -d ~/.local/share/nvim/lazy/ ]] && echo "$lazy_dir has been deleted. Please reinstall all plugins again."
+}
