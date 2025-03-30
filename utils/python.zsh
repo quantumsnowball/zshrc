f941fb1f7401e709bdc428772aeec79bd02c72d9
Quantum Snowball <quantum.snowball@gmail.com>
2025-03-30 18:56:31 +0800

    pydebug

--- utils/python.zsh
+++ utils/python.zsh
@@ -7,7 +7,12 @@ alias ipy=ipython
 alias ptpy=ptpython
 alias ptipy=ptipython
 
+# debugger
+alias py.debug.pdb='python -m pdb'
+alias py.debug.pudb='python -m pudb'
+alias pyd=py.debug.pudb
 
+# clean pycache
 pyclean () {
     find . -type f -name '*.py[co]' -delete -o -type d -name __pycache__ -delete
 }
