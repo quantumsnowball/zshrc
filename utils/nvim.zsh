51108d8945f45719e957fa5c8bd09959cac0fd67
Quantum Snowball <quantum.snowball@gmail.com>
2025-04-01 15:10:10 +0800

    fix wrong logic

--- utils/nvim.zsh
+++ utils/nvim.zsh
@@ -6,8 +6,8 @@ alias vi='nvim'
 alias v='nvim'
 
 nvim.lazy.rm-all-plugins () {
-    $lazy_dir=~/.local/share/nvim/lazy/
+    lazy_dir=~/.local/share/nvim/lazy/
     read -q "confirm?Are you sure you want to remove all lazy plugins? (y/n): "
     [[ "$confirm" =~ ^[Yy]$ ]] && rm -rf $lazy_dir
-    [[ -d ~/.local/share/nvim/lazy/ ]] && echo "$lazy_dir has been deleted. Please reinstall all plugins again."
+    [[ -d ~/.local/share/nvim/lazy/ ]] || echo "\n$lazy_dir has been deleted. Please reinstall all plugins again."
 }
