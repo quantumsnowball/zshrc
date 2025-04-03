80b595204964c8e3a245c73fcb655564f328d54b
Quantum Snowball <quantum.snowball@gmail.com>
2025-04-03 19:18:17 +0800

    info.raw should not use other binary

--- utils/ffprobe/streams.zsh
+++ utils/ffprobe/streams.zsh
@@ -5,11 +5,14 @@ ffprobe-streams-old()
     python $HOME/.config/zshrc/utils/ffprobe/streams.py "$@"
 }
 
-ffprobe.streams.raw-info () {
+ffprobe.streams.info.raw () {
     ffprobe \
         -v quiet \
         -output_format json \
         -show_streams \
         -hide_banner\
         $1
+}
+ffprobe.streams.info () {
+    ffprobe.streams.info.raw $1 | jq '.streams.[]'
 }
