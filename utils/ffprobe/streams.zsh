1837dab11b3e54eeefca727bb876475b6d0ded8f
Quantum Snowball <quantum.snowball@gmail.com>
2025-04-03 19:05:20 +0800

    raw-info

--- utils/ffprobe/streams.zsh
+++ utils/ffprobe/streams.zsh
@@ -5,11 +5,11 @@ ffprobe-streams-old()
     python $HOME/.config/zshrc/utils/ffprobe/streams.py "$@"
 }
 
-ffprobe.streams () {
+ffprobe.streams.raw-info () {
     ffprobe \
         -v quiet \
         -output_format json \
         -show_streams \
         -hide_banner\
-        $1 | jq
+        $1
 }
