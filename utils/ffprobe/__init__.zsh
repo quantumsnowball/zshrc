762d6c2fa74beb96746d43e5fd5e8eb9c1f5fe03
Quantum Snowball <quantum.snowball@gmail.com>
2025-04-03 18:57:41 +0800

    recreate the original ffprobe script

--- utils/ffprobe/__init__.zsh
+++ utils/ffprobe/__init__.zsh
@@ -4,5 +4,10 @@ ensure ffprobe || return
 source $HOME/.config/zshrc/utils/ffprobe/streams.zsh
 
 ffprobe-streams () {
-    echo 'rewrite ffprobe.streams without depending on python'
+    ffprobe \
+        -v quiet \
+        -output_format json \
+        -show_streams \
+        -hide_banner\
+        $1
 }
