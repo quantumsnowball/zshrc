4bf321277bc32b9928f8690cf86b3f37322fc846
Quantum Snowball <quantum.snowball@gmail.com>
2025-04-03 18:59:36 +0800

    fix wrong new function position

--- utils/ffprobe/__init__.zsh
+++ utils/ffprobe/__init__.zsh
@@ -3,11 +3,3 @@ ensure ffprobe || return
 
 source $HOME/.config/zshrc/utils/ffprobe/streams.zsh
 
-ffprobe-streams () {
-    ffprobe \
-        -v quiet \
-        -output_format json \
-        -show_streams \
-        -hide_banner\
-        $1
-}
--- utils/ffprobe/streams.zsh
+++ utils/ffprobe/streams.zsh
@@ -5,3 +5,11 @@ ffprobe.streams()
     python $HOME/.config/zshrc/utils/ffprobe/streams.py "$@"
 }
 
+ffprobe-streams () {
+    ffprobe \
+        -v quiet \
+        -output_format json \
+        -show_streams \
+        -hide_banner\
+        $1
+}
