771ecf4c215d1869d6a05ca84c6b86d5467a9625
Quantum Snowball <quantum.snowball@gmail.com>
2025-04-05 00:12:34 +0800

    pipx

---
+++ utils/pipx.zsh
@@ -0,0 +1,20 @@
+ensure pipx || return
+
+
+alias pipx.run='pipx run'
+alias pipxrun=pipx.run
+
+alias pipx.install='pipx install'
+alias pipxi=pipx.install
+
+alias pipx.ls='pipx list'
+alias pipxls=pipx.ls
+
+alias pipx.remove='pipx uninstall'
+alias pipxrm=pipx.remove
+
+alias pipx.update='pipx upgrade'
+alias pipxu=pipx.update
+
+alias pipx.upgrade-all='pipx upgrade-all'
+alias pipxup=pipx.upgrade-all
