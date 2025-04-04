206826d360434cba406743a8aa233c50c82f136e
Quantum Snowball <quantum.snowball@gmail.com>
2025-04-05 05:13:15 +0800

    notes

--- init/virtualenv.zsh
+++ init/virtualenv.zsh
@@ -11,4 +11,13 @@ fi
 # to create a new venv, run:
 # (make sure include --system-site-packages flag to include pkg/apt packages)
 #   virtualenv --system-site-packages ~/.python/virtualenv/<env-name>
+#
+# to install virtualenv, use pipx:
+#   pipx install virtualenv
+# to install pipx:
+#   apt install pipx, or use python -m pip install --user pipx
+# if system python refuse to install by pip, try create venv:
+#   python -m venv ~/.python/venv/pipx
+#   then install pipx using this venv's python
+#   (may require --system-site-packages flag if no access)
 
