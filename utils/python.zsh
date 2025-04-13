11d9a85d6db1ca16203b75dff70b4f44bc87ef84
Quantum Snowball <quantum.snowball@gmail.com>
2025-04-14 03:27:20 +0800

    breakpoint() use pudb

--- utils/python.zsh
+++ utils/python.zsh
@@ -12,6 +12,9 @@ alias py.debug.pdb='python -m pdb'
 alias py.debug.pudb='python -m pudb'
 alias pyd=py.debug.pudb
 
+# breakpoint() use pudb
+export PYTHONBREAKPOINT=pudb.set_trace
+
 # clean pycache
 pyclean () {
     find . -type f -name '*.py[co]' -delete -o -type d -name __pycache__ -delete
