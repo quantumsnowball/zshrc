de421e4672518843378a7301af557f424fd838e0
Quantum Snowball <quantum.snowball@gmail.com>
2025-05-12 19:29:51 +0800

    rg.less

--- utils/rg.zsh
+++ utils/rg.zsh
@@ -1,1 +1,15 @@
 ensure rg || return
+
+
+rg.less () {
+    rg \
+        --color=always \
+        --heading \
+        --colors='path:fg:yellow' \
+        "$1" | 
+    # c - clear screen
+    # S - no wrap
+    # R - raw char with color
+    # N - print line number
+    less -cSRN
+}
