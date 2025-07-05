e40632f161a31fd1d469d813117e5e4e74eb2fb2
Quantum Snowball <quantum.snowball@gmail.com>
2025-07-06 07:43:09 +0800

    basic

---
+++ utils/ffmpeg/loop.zsh
@@ -0,0 +1,49 @@
+ffmpeg.loop()
+{
+  local usage="Usage: ffmpeg.cut -ss/--start START -to/--end END -i/--input INPUT OUTPUT"
+
+  # parse opts
+  local input ss to output
+  while [[ $# -gt 0 ]]; do
+    case $1 in
+      -i|--input)
+        input="$2"
+        shift # past argument
+        shift # past value
+        ;;
+      -ss|--start)
+        ss="$2"
+        shift # past argument
+        shift # past value
+        ;;
+      -to|--end)
+        to="$2"
+        shift # past argument
+        shift # past value
+        ;;
+      -*|--*)
+        echo "Unknown option $1"
+        return 1
+        ;;
+      *)
+        # ensure only set output once
+        if [[ -n $output ]]; then
+          echo 'Error: Only accept one positional argument as output filename'
+          echo $usage
+          return 1
+        fi
+        output="$1" # first positional args
+        shift # past argument
+        ;;
+    esac
+  done
+
+  # ensure all options and arg exist
+  if [[ -z $input || -z $ss || -z $to || -z $output ]]; then
+    echo $usage
+    return 1
+  fi
+
+  # 
+  echo ffmpeg.loop
+}
