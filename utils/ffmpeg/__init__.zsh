9b896d0bfa991e8244b3db20de50a5f2be5c4ab8
Quantum Snowball <quantum.snowball@gmail.com>
2025-08-30 20:36:08 +0800

    include

--- utils/ffmpeg/__init__.zsh
+++ utils/ffmpeg/__init__.zsh
@@ -5,3 +5,4 @@ source $HOME/.config/zshrc/utils/ffmpeg/concat.zsh
 source $HOME/.config/zshrc/utils/ffmpeg/embed-subtitles.zsh
 source $HOME/.config/zshrc/utils/ffmpeg/cut.zsh
 source $HOME/.config/zshrc/utils/ffmpeg/loop.zsh
+source $HOME/.config/zshrc/utils/ffmpeg/set-framerate.zsh
