9f0eb402b926b9055ed2b6102b1ccb60f81723cb
Quantum Snowball <quantum.snowball@gmail.com>
2025-10-04 19:34:31 +0800

    these env is for gemini api

--- init/env.zsh
+++ init/env.zsh
@@ -3,3 +3,11 @@
 ensure bat && export GIT_PAGER="bat"
 # editor
 ensure nvim && export EDITOR="nvim"
+# gemini
+if [[ -f ~/.gemini ]]; then
+    source ~/.gemini
+fi
+# proxy
+if [[ -f ~/.http-proxy ]]; then
+    source ~/.http-proxy
+fi
