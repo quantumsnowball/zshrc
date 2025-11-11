854255ef07cba302098db975d33b14541de4e388
Quantum Snowball <quantum.snowball@gmail.com>
2025-11-12 02:43:02 +0800

    define more sub types

--- utils/ffprobe/lib/ffprobe_streams/result.py
+++ utils/ffprobe/lib/ffprobe_streams/result.py
@@ -1,18 +1,38 @@
+from abc import ABC, abstractproperty
 from typing import Any
+
+type Data = dict[str, Any]
+
+
+class Stream(ABC):
+    def __init__(self, data: Data) -> None:
+        self._d = data
+
+
+class VideoStream(Stream):
+    pass
+
+
+class AudioStream(Stream):
+    pass
 
 
 class Result:
-    def __init__(self, data: dict[str, Any]) -> None:
+    def __init__(self, data: Data) -> None:
         self._d = data
-        # from pprint import pprint
-        # pprint(self._d)
 
     @property
-    def streams(self) -> dict[str, Any]:
-        return self._d['streams']
+    def streams(self) -> list[VideoStream | AudioStream]:
+        streams = []
+        for s in self._d['streams']:
+            if s["codec_type"] == "video":
+                streams.append(VideoStream(s))
+            elif s["codec_type"] == "audio":
+                streams.append(AudioStream(s))
+        return streams
 
     @property
-    def format(self) -> dict[str, Any]:
+    def format(self) -> Data:
         return self._d['format']
 
     @property
