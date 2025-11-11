fc25f0759914c1670269c905dc237b4923d5b171
Quantum Snowball <quantum.snowball@gmail.com>
2025-11-12 03:30:55 +0800

    apply color

--- utils/ffprobe/lib/ffprobe_streams/present/__init__.py
+++ utils/ffprobe/lib/ffprobe_streams/present/__init__.py
@@ -1,8 +1,17 @@
+from ffprobe_streams.lib.ansi_colors import GREEN, MAGENTA, RESET, YELLOW
 from ffprobe_streams.present.audio import present_audio
 from ffprobe_streams.present.video import present_video
 from ffprobe_streams.result import Result
+from ffprobe_streams.result.stream import Stream
 from ffprobe_streams.result.stream.audio import AudioStream
 from ffprobe_streams.result.stream.video import VideoStream
+
+
+def present_stream_title(i: int, s: Stream) -> None:
+    field = f'{GREEN}Stream {i}{RESET}'
+    color = YELLOW if s.codec_type == 'video' else MAGENTA
+    content = f'{color}{s.codec_type} []{RESET}'
+    print(f'{field}: {content}')
 
 
 def present(r: Result) -> None:
@@ -10,7 +19,7 @@ def present(r: Result) -> None:
     print(f"\n\t< ffprobe: total {r.nb_streams} stream(s) >\n",)
     # present streams
     for i, s in enumerate(r.streams):
-        print(f'Stream {i}: [{s.codec_type}]')
+        present_stream_title(i, s)
         if isinstance(s, VideoStream):
             present_video(s)
         elif isinstance(s, AudioStream):
