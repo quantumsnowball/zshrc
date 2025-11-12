0cfe649f064fa5b88083d98e89894027178c4575
Quantum Snowball <quantum.snowball@gmail.com>
2025-11-12 14:19:02 +0800

    delegate

--- utils/ffprobe/lib/ffprobe_streams/present/__init__.py
+++ utils/ffprobe/lib/ffprobe_streams/present/__init__.py
@@ -1,22 +1,14 @@
-from ffprobe_streams.lib.ansi_colors import GREEN, MAGENTA, RESET, YELLOW
 from ffprobe_streams.present.audio import present_audio
+from ffprobe_streams.present.title import present_stream_title, present_subject
 from ffprobe_streams.present.video import present_video
 from ffprobe_streams.result import Result
-from ffprobe_streams.result.stream import Stream
 from ffprobe_streams.result.stream.audio import AudioStream
 from ffprobe_streams.result.stream.video import VideoStream
 
 
-def present_stream_title(i: int, s: Stream) -> None:
-    field = f'{GREEN}Stream {i}{RESET}'
-    color = YELLOW if s.codec_type == 'video' else MAGENTA
-    content = f'{color}{s.codec_type} []{RESET}'
-    print(f'{field}: {content}')
-
-
 def present(r: Result) -> None:
     # subject line
-    print(f"\n\t< ffprobe: total {r.nb_streams} stream(s) >\n",)
+    present_subject(r.nb_streams)
     # present streams
     for i, s in enumerate(r.streams):
         present_stream_title(i, s)
---
+++ utils/ffprobe/lib/ffprobe_streams/present/title.py
@@ -0,0 +1,13 @@
+from ffprobe_streams.lib.ansi_colors import GREEN, MAGENTA, RESET, YELLOW
+from ffprobe_streams.result.stream import Stream
+
+
+def present_subject(nb_streams: int) -> None:
+    print(f"\n\t< ffprobe: total {nb_streams} stream(s) >\n",)
+
+
+def present_stream_title(i: int, s: Stream) -> None:
+    field = f'{GREEN}Stream {i}{RESET}'
+    color = YELLOW if s.codec_type == 'video' else MAGENTA
+    content = f'{color}{s.codec_type} []{RESET}'
+    print(f'{field}: {content}')
