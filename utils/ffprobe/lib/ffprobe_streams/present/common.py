d3416f71848ee141a7e51533c1c02b8ba51df7b7
Quantum Snowball <quantum.snowball@gmail.com>
2025-11-12 15:40:19 +0800

    field value helper

--- utils/ffprobe/lib/ffprobe_streams/present/common.py
+++ utils/ffprobe/lib/ffprobe_streams/present/common.py
@@ -2,6 +2,10 @@ from ffprobe_streams.lib.ansi_colors import *
 from ffprobe_streams.result.stream import Stream
 
 
+def print_field_value(field: str, value: str, *, tab: bool = True) -> None:
+    print(f'{'\t' if tab else ''}{field}: {value}')
+
+
 def present_subject(nb_streams: int) -> None:
     print(f"\n\t< ffprobe: total {nb_streams} stream(s) >\n",)
 
@@ -9,12 +13,12 @@ def present_subject(nb_streams: int) -> None:
 def present_stream_title(i: int, s: Stream) -> None:
     field = f'{GREEN}Stream {i}{RESET}'
     color = YELLOW if s.codec_type == 'video' else MAGENTA
-    content = f'{color}{s.codec_type} [{s.duration_hms}]{RESET}'
-    print(f'{field}: {content}')
+    value = f'{color}{s.codec_type} [{s.duration_hms}]{RESET}'
+    print_field_value(field, value, tab=False)
 
 
 def present_codec(s: Stream) -> None:
     field = f'{BLUE}codec{RESET}'
-    content = f'{RED}{s.codec_name}{RESET}'
+    value = f'{RED}{s.codec_name}{RESET}'
     detail = f'{WHITE}{s.codec_long_name}{RESET}'
-    print(f'\t{field}: {content} ({detail})')
+    print_field_value(field, f'{value} ({detail})')
