34c6181f3df995cee5cee62db13593301f31c762
Quantum Snowball <quantum.snowball@gmail.com>
2025-11-12 14:58:13 +0800

    duration_hms

--- utils/ffprobe/lib/ffprobe_streams/result/stream/__init__.py
+++ utils/ffprobe/lib/ffprobe_streams/result/stream/__init__.py
@@ -19,3 +19,24 @@ class Stream(ABC):
                 self._d['tags'].get('DURATION')
         except KeyError:
             return None
+
+    @property
+    def duration_hms(self) -> str | None:
+        dur = self.duration
+        # guard
+        if not dur:
+            return None
+        # if duration is already HMS format, return as is
+        if ':' in dur:
+            return dur.split('.')[0]
+        # if duration is a float str, conver to HMS string
+        try:
+            total_seconds = float(dur)
+        except ValueError:
+            return None
+        hours = int(total_seconds // 3600)
+        remaining_seconds_after_hours = total_seconds % 3600
+        minutes = int(remaining_seconds_after_hours // 60)
+        seconds = int(remaining_seconds_after_hours % 60)
+        hms = f"{hours:02d}:{minutes:02d}:{seconds:02d}"
+        return hms
