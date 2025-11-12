e923d29ee911ee5327a679edc16b091a4f71430d
Quantum Snowball <quantum.snowball@gmail.com>
2025-11-12 15:43:37 +0800

    resolution

--- utils/ffprobe/lib/ffprobe_streams/present/video.py
+++ utils/ffprobe/lib/ffprobe_streams/present/video.py
@@ -1,7 +1,15 @@
+from ffprobe_streams.lib.ansi_colors import *
 from ffprobe_streams.present.common import *
 from ffprobe_streams.result.stream.video import VideoStream
+
+
+def present_resolution(s: VideoStream) -> None:
+    field = f'{BLUE}codec{RESET}'
+    value = f'{WHITE}{s.resolution}{RESET}'
+    print_field_value(field, value)
 
 
 def present(i: int, s: VideoStream) -> None:
     present_stream_title(i, s)
     present_codec(s)
+    present_resolution(s)
--- utils/ffprobe/lib/ffprobe_streams/result/stream/video.py
+++ utils/ffprobe/lib/ffprobe_streams/result/stream/video.py
@@ -2,4 +2,14 @@ from ffprobe_streams.result.stream import Stream
 
 
 class VideoStream(Stream):
-    pass
+    @property
+    def width(self) -> str:
+        return self._d['width']
+
+    @property
+    def height(self) -> str:
+        return self._d['height']
+
+    @property
+    def resolution(self) -> str:
+        return f'{self.width} x {self.height} px'
