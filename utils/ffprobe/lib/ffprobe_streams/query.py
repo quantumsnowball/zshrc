98fb1e29c33b183ada05824cdcc9ceb0e7473dde
Quantum Snowball <quantum.snowball@gmail.com>
2025-11-12 20:03:29 +0800

    query use object_hook=defaultdict with default value set to None

--- utils/ffprobe/lib/ffprobe_streams/query.py
+++ utils/ffprobe/lib/ffprobe_streams/query.py
@@ -1,7 +1,9 @@
 import json
 import subprocess
+from collections import defaultdict
+from functools import partial
 from pathlib import Path
-from typing import Any
+from typing import Any, DefaultDict
 
 from ffprobe_streams.result import Result
 
@@ -19,6 +21,6 @@ def ffprobe(path: Path) -> Result:
     # run ffprobe as process
     result = subprocess.run(cmd, capture_output=True, text=True, check=True)
     # parse json result
-    data: dict[str, Any] = json.loads(result.stdout)
+    data: DefaultDict[str, Any] = json.loads(result.stdout, object_hook=partial(defaultdict, lambda: None))
     # return result
     return Result(data)
