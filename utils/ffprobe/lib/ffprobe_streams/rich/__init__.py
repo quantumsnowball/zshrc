171073e51c40ac89da9799d0d16ea133fb924552
Quantum Snowball <quantum.snowball@gmail.com>
2025-11-20 02:15:40 +0800

    better naming

--- utils/ffprobe/lib/ffprobe_streams/rich/__init__.py
+++ utils/ffprobe/lib/ffprobe_streams/rich/__init__.py
@@ -12,54 +12,49 @@ from rich.table import Table
 class RichTable:
     def __init__(self, r: Result) -> None:
         self._r = r
-        self._table = Table()
-
-        # helper
-        self.add_column = self._table.add_column
-        self.add_row = self._table.add_row
-        self.add_section = self._table.add_section
+        self._t = Table()
 
     def present(self) -> None:
         # caption
         g = General(self._r)
-        self._table.title = g.title
+        self._t.title = g.title
 
         # format
         f = Format(self._r)
-        self.add_column(f.title.field)
-        self.add_column(f.title.value)
-        self.add_row(*f.size.tuple)
-        self.add_row(*f.name.tuple)
-        self.add_row(*f.bit_rate.tuple)
+        self._t.add_column(f.title.field)
+        self._t.add_column(f.title.value)
+        self._t.add_row(*f.size.tuple)
+        self._t.add_row(*f.name.tuple)
+        self._t.add_row(*f.bit_rate.tuple)
 
         # present streams
         for i, s in enumerate(self._r.streams):
             # video stream
             if isinstance(s, VideoStream):
                 v = Video(s)
-                self.add_section()
-                self.add_row(*v.title(i).tuple)
-                self.add_section()
-                self.add_row(*v.codec.tuple)
-                self.add_row(*v.resolution.tuple)
-                self.add_row(*v.sample_aspect_ratio.tuple)
-                self.add_row(*v.display_aspect_ratio.tuple)
-                self.add_row(*v.r_frame_rate.tuple)
-                self.add_row(*v.avg_frame_rate.tuple)
-                self.add_row(*v.nb_frames.tuple)
-                self.add_row(*v.bit_rate.tuple)
-                self.add_row(*v.language.tuple)
+                self._t.add_section()
+                self._t.add_row(*v.title(i).tuple)
+                self._t.add_section()
+                self._t.add_row(*v.codec.tuple)
+                self._t.add_row(*v.resolution.tuple)
+                self._t.add_row(*v.sample_aspect_ratio.tuple)
+                self._t.add_row(*v.display_aspect_ratio.tuple)
+                self._t.add_row(*v.r_frame_rate.tuple)
+                self._t.add_row(*v.avg_frame_rate.tuple)
+                self._t.add_row(*v.nb_frames.tuple)
+                self._t.add_row(*v.bit_rate.tuple)
+                self._t.add_row(*v.language.tuple)
             # audio stream
             elif isinstance(s, AudioStream):
                 a = Audio(s)
-                self.add_section()
-                self.add_row(*a.title(i).tuple)
-                self.add_section()
-                self.add_row(*a.codec.tuple)
-                self.add_row(*a.bit_rate.tuple)
-                self.add_row(*a.sample_rate.tuple)
-                self.add_row(*a.language.tuple)
+                self._t.add_section()
+                self._t.add_row(*a.title(i).tuple)
+                self._t.add_section()
+                self._t.add_row(*a.codec.tuple)
+                self._t.add_row(*a.bit_rate.tuple)
+                self._t.add_row(*a.sample_rate.tuple)
+                self._t.add_row(*a.language.tuple)
 
         # print
         console = Console()
-        console.print(self._table)
+        console.print(self._t)
