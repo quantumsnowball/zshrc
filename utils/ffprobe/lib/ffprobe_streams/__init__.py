d5313691058f12ba5e2412b56d47edbff09eb296
Quantum Snowball <quantum.snowball@gmail.com>
2025-11-20 02:30:57 +0800

    cleanup: don't use present_old

--- utils/ffprobe/lib/ffprobe_streams/__init__.py
+++ utils/ffprobe/lib/ffprobe_streams/__init__.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 
-from ffprobe_streams.present import present as present_old
 from ffprobe_streams.query import ffprobe
 from ffprobe_streams.rich import RichTable
 
@@ -9,5 +8,4 @@ def main(path: Path):
     # query ffprobe
     result = ffprobe(path)
     # present
-    present_old(result)
     RichTable(result).present()
