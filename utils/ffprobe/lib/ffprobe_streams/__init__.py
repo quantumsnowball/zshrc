0f1968b014c007a6279f0eb75b1d86325d135a81
Quantum Snowball <quantum.snowball@gmail.com>
2025-11-19 19:15:24 +0800

    create rich package

--- utils/ffprobe/lib/ffprobe_streams/__init__.py
+++ utils/ffprobe/lib/ffprobe_streams/__init__.py
@@ -1,11 +1,13 @@
 from pathlib import Path
 
-from ffprobe_streams.present import present
+from ffprobe_streams.present import present as present_old
 from ffprobe_streams.query import ffprobe
+from ffprobe_streams.rich import present
 
 
 def main(path: Path):
     # query ffprobe
     result = ffprobe(path)
     # present
+    present_old(result)
     present(result)
---
+++ utils/ffprobe/lib/ffprobe_streams/rich/__init__.py
@@ -0,0 +1,5 @@
+from ffprobe_streams.result import Result
+
+
+def present(r: Result) -> None:
+    print('gonna use the rich library here')
