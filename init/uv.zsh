ef684635cb701d1afbcfd21879962c790ed2e28b
Quantum Snowball <quantum.snowball@gmail.com>
2025-11-21 06:35:54 +0800

    auto activate default venv

---
+++ init/uv.zsh
@@ -0,0 +1,11 @@
+ensure uv || return
+
+
+# default venv
+# please create the file ~/.uv/default-venv and put the venv name in it
+UV_DIR=$HOME/.uv
+VENV_DIR=$UV_DIR/venv/
+DEFAULT_VENV_FILE=$UV_DIR/default-venv
+VENV_NAME=$(cat "$DEFAULT_VENV_FILE")
+VENV_PATH="$HOME/.uv/venv/$VENV_NAME/bin/activate"
+[[ -f "$VENV_PATH" ]] && source "$VENV_PATH"
