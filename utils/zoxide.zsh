8325daf435c5057bb195653b4f86335250080d80
Quantum Snowball <quantum.snowball@gmail.com>
2025-11-21 05:16:56 +0800

    not useful

--- utils/zoxide.zsh
+++ utils/zoxide.zsh
@@ -4,11 +4,4 @@ ensure zoxide || return
 # zoxide
 # alias z="__zoxide_z" # disabled: this breaks z <Tab> completion
 alias zz="__zoxide_zi"
-alias Z="__zoxide_zi"
 alias zl="z -"
-
-
-# z then v
-zv() {
-  z $1 && nvim;
-}
