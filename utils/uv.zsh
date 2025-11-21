67d7a8b2d78ae86815582dc0d78b6e8762d093c0
Quantum Snowball <quantum.snowball@gmail.com>
2025-11-21 20:21:29 +0800

    uv.disk-usage

--- utils/uv.zsh
+++ utils/uv.zsh
@@ -17,3 +17,24 @@ uv.venv.activate-local () {
         echo "Error: ./.venv/bin/activate not found. No virtual environment activated."
     fi
 }
+
+# disk usage
+uv.disk-usage () {
+    echo "=== Real uv disk usage ==="
+    
+    # Cache
+    echo "Cache (real wheels + extracted files):"
+    du -sh ~/.cache/uv || echo "cache is empty"
+    
+    # tools
+    echo -e "\nHard-linked tools environments:"
+    fd -u -t d -d 1 . ~/.local/share/uv/tools -X du -sh --apparent-size | sort -h
+
+    # user envs
+    echo -e "\nHard-linked user environments:"
+    fd -u -t d -d 1 . ~/.uv/venv -X du -sh --apparent-size | sort -h
+    
+    # project envs under repos
+    echo -e "\nProject .venv directories:"
+    fd -u -t d '^\.venv$' ~/repos -X du -sh --apparent-size | sort -h
+}
