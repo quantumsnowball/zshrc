6bd8e65a672670bd04d2aa7346d94e3b9b17a667
Quantum Snowball <quantum.snowball@gmail.com>
2025-12-24 04:04:03 +0800

    ffmpeg re-encode utils

--- utils/ffmpeg/timescale.zsh
+++ utils/ffmpeg/timescale.zsh
@@ -1,3 +1,8 @@
-ffmpeg.to-60fps-timescale90000() {
-    ffmpeg -i $1 -c copy -video_track_timescale 90000 $2
+ffmpeg.to-60fps-timescale90000-copy() {
+    ffmpeg -i "$1" -c copy -video_track_timescale 90000 "$2"
 }
+
+ffmpeg.to-60fps-timescale90000-libx264-aac() {
+    ffmpeg -i "$1" -c:v libx264 -c:a aac -video_track_timescale 90000 "$2"
+}
+
