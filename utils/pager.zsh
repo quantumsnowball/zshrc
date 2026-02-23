92ef61902859d7ded4ed10dfcb211c8d249571f0
Quantum Snowball <quantum.snowball@gmail.com>
2026-02-24 07:06:15 +0800

    use a subshell, so the exports won't pollute the current shell vars

--- utils/pager.zsh
+++ utils/pager.zsh
@@ -1,21 +1,24 @@
 pager() {
-    # Force colors for standard tools and Python/Ruff
-    export CLICOLOR_FORCE=1
-    export PY_COLORS=1
-    export FORCE_COLOR=1 
+    # use a subshell, so the exports won't pollute the current shell vars
+    (
+        # Force colors for standard tools and Python/Ruff
+        export CLICOLOR_FORCE=1
+        export PY_COLORS=1
+        export FORCE_COLOR=1 
 
-    # Run the command and pipe to less
-    # -c: Clear screen before painting
-    # -S: Chop long lines (prevents wrapping mess)
-    # -R: Repaint (shows colors)
-    
-    # if first word is an alias, run in newe zsh interactitve shell and get output
-    if alias "$1" >/dev/null; then
-        # quote and escape the argumnt with printf
-        output=$(zsh -i -c "$(printf '%q ' "$@")" 2>&1) 
-        echo "$output" | less -cSR
-    # if not an alias, then just run it in current shell as a normal command
-    else
-        "$@" 2>&1 | less -cSR
-    fi
+        # Run the command and pipe to less
+        # -c: Clear screen before painting
+        # -S: Chop long lines (prevents wrapping mess)
+        # -R: Repaint (shows colors)
+        
+        # if first word is an alias, run in newe zsh interactitve shell and get output
+        if alias "$1" >/dev/null; then
+            # quote and escape the argumnt with printf
+            output=$(zsh -i -c "$(printf '%q ' "$@")" 2>&1) 
+            echo "$output" | less -cSR
+        # if not an alias, then just run it in current shell as a normal command
+        else
+            "$@" 2>&1 | less -cSR
+        fi
+    )
 }
