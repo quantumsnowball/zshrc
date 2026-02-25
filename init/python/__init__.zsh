4da1f3fdb75bbc0867012bf90751bd132d7263bc
Quantum Snowball <quantum.snowball@gmail.com>
2026-02-26 02:26:19 +0800

    alias env.python.enable-verbose='PYTHONVERBOSE=1'

--- init/python/__init__.zsh
+++ init/python/__init__.zsh
@@ -4,3 +4,6 @@ export PYTHONSTARTUP=$HOME/.config/zshrc/init/python/pythonstartup.py
 # inject smart breakpoint
 export PYTHONPATH=$HOME/.config/zshrc/init/python/pythonpath:$PYTHONPATH
 export PYTHONBREAKPOINT=smart_breakpoint.set_trace
+
+# env helpers
+alias env.python.enable-verbose='PYTHONVERBOSE=1'
