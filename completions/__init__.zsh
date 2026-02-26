af24ab96ea3c7c2e240a18569d078d91d72dd8e7
Quantum Snowball <quantum.snowball@gmail.com>
2026-02-26 22:17:42 +0800

    handy tab toggle completion

--- completions/__init__.zsh
+++ completions/__init__.zsh
@@ -26,7 +26,8 @@ bindkey -M menuselect 'h' vi-backward-char
 bindkey -M menuselect 'k' vi-up-line-or-history
 bindkey -M menuselect 'j' vi-down-line-or-history
 bindkey -M menuselect 'l' vi-forward-char
-bindkey -M menuselect '\e' send-break
+bindkey -M menuselect '^i' send-break # tab
+bindkey -M menuselect '\e' send-break # escape
 bindkey -M menuselect 'q' send-break
 
 # details
