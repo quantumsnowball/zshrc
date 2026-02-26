7c9604e1fb509f13dbb233dd5821c2f6b1f18c55
Quantum Snowball <quantum.snowball@gmail.com>
2026-02-26 22:12:46 +0800

    fix loading order

--- __init__.zsh
+++ __init__.zsh
@@ -2,6 +2,6 @@ source $HOME/.config/zshrc/pre.zsh
 source $HOME/.config/zshrc/paths/__init__.zsh
 source $HOME/.config/zshrc/init/__init__.zsh
 source $HOME/.config/zshrc/plugins/__init__.zsh
-source $HOME/.config/zshrc/utils/__init__.zsh
 source $HOME/.config/zshrc/completions/__init__.zsh
+source $HOME/.config/zshrc/utils/__init__.zsh
 source $HOME/.config/zshrc/post.zsh
