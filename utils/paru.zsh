0334f7d2a4f5837e4596e1ebec6a84928c380699
Quantum Snowball <quantum.snowball@gmail.com>
2026-03-10 03:28:57 +0800

    paru

---
+++ utils/paru.zsh
@@ -0,0 +1,25 @@
+ensure paru || return
+
+
+# paru
+alias pr=paru
+
+alias pr.install='sudo paru -S'
+alias pri=pr.install
+
+alias prs='paru -Ss'
+
+alias pr.ls='paru -Q'
+alias prls=paru.ls
+
+alias pr.ls-grep='paru -Q | rg'
+alias prrg=pr.ls-grep
+
+alias pr.remove='paru -Rsu'
+alias prrm=pr.remove
+
+alias pr.update='paru -Sy && paru -Qu'
+alias pru=pr.update
+
+alias pr.upgrade='paru -Syu'
+alias prup=pr.upgrade
