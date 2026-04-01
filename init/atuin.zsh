a6209a6019aae8c6653b49e179a2f131b9481cec
Quantum Snowball <quantum.snowball@gmail.com>
2026-04-01 20:24:52 +0800

    better atuin hostname for Termux and WSL

--- init/atuin.zsh
+++ init/atuin.zsh
@@ -1,6 +1,11 @@
 ensure atuin || return
 
 
+# Termux doesn't have a hostname, it defaults to 'localhost', so manually make one for atuin
+[ -v TERMUX_VERSION ] && export ATUIN_HOST_NAME="Termux"
+# For WSL, better use the distro name as hostname to distinguish from the Win host's hostname
+[ -v WSL_DISTRO_NAME ] && export ATUIN_HOST_NAME="$WSL_DISTRO_NAME"
+
 # docs: https://docs.atuin.sh/cli/configuration/key-binding
 # usage: eval "$(atuin init zsh --disable-up-arrow --disable-ctrl-r)"
 eval "$(atuin init zsh --disable-up-arrow)"
