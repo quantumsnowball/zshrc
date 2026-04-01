d2a980de0897ddb77153abd7d38a539058854421
Quantum Snowball <quantum.snowball@gmail.com>
2026-04-01 18:52:42 +0800

    correct way for zsh to write history

--- init/history.zsh
+++ init/history.zsh
@@ -1,9 +1,17 @@
-# Lines configured by zsh-newuser-install
-HISTFILE=~/.histfile
-HISTSIZE=1000000
-SAVEHIST=1000000
-# End of lines configured by zsh-newuser-install
+# The path to your history file
+HISTFILE=$HOME/.zsh_history
 
-# share shell command history immediately between shells
+# Max number of commands kept in the internal list
+HISTSIZE=10000000
+
+# Max number of commands saved in the history file
+SAVEHIST=10000000
+
+# Save the time the command was started and the duration
+setopt EXTENDED_HISTORY
+
+# Immediately append to the history file, don't wait for the shell to exit
+setopt INC_APPEND_HISTORY 
+
+# Share history between all open terminal sessions
 setopt SHARE_HISTORY
-
