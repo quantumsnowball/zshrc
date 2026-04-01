6833e40462ba192977fee0e049aa8e5e61babe2d
Quantum Snowball <quantum.snowball@gmail.com>
2026-04-02 03:06:45 +0800

    lets try the custom up arrow invoke

--- init/atuin.zsh
+++ init/atuin.zsh
@@ -9,3 +9,7 @@ ensure atuin || return
 # docs: https://docs.atuin.sh/cli/configuration/key-binding
 # usage: eval "$(atuin init zsh --disable-up-arrow --disable-ctrl-r)"
 eval "$(atuin init zsh --disable-up-arrow)"
+
+# binds
+bindkey -M vicmd 'K' atuin-search-vicmd
+bindkey -M vicmd 'J' atuin-search-viins
