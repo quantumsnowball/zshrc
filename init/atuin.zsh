d8b978d0b8d3c84e048d020e3c44a29504c22146
Quantum Snowball <quantum.snowball@gmail.com>
2026-04-01 16:25:38 +0800

    use atuin to replace .histfile and fzf

---
+++ init/atuin.zsh
@@ -0,0 +1,6 @@
+ensure atuin || return
+
+
+# docs: https://docs.atuin.sh/cli/configuration/key-binding
+# usage: eval "$(atuin init zsh --disable-up-arrow --disable-ctrl-r)"
+eval "$(atuin init zsh --disable-up-arrow)"
--- plugins/fzf.zsh
+++
@@ -1,5 +0,0 @@
-# fzf
-# Modified command to work with zsh-vi-mode plugins
-zinit ice lucid wait
-ensure fzf && zinit snippet OMZP::fzf
-
