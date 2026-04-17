0e90a993cd6caefd6c957b62bd73819648d86f8e
Quantum Snowball <quantum.snowball@gmail.com>
2026-04-18 05:43:58 +0800

    refactor: reorganize systemctl and system aliases
    - Split service-related aliases into separate sysd.system and sysd.user prefixes for clearer namespace separation
    - Added dedicated user and system categories for logs, restart, reload, start, stop, enable, disable, daemon-reload, and edit commands

--- utils/systemd.zsh
+++ utils/systemd.zsh
@@ -3,43 +3,49 @@ installed systemctl || return
 
 # systemctl
 alias sysd.status='systemctl status'
-# ls
-alias sysd.ls='sysd.ls.service.running'
-alias sysd.ls.service.loaded='systemctl list-units --type=service'
-alias sysd.ls.service.running-system='systemctl list-units --type=service --state=running'
-alias sysd.ls.service.running-user='systemctl --user list-units --type=service --state=running'
-sysd.ls.service.running() {
-    sysd.ls.service.running-system 
-    echo '\n'
-    sysd.ls.service.running-user
-}
-alias sysd.ls.service.failed='systemctl list-units --type=service --state=failed'
-alias sysd.ls.service.installed='systemctl list-unit-files --type=service'
-alias sysd.ls.service.enabled='systemctl list-unit-files --type=service --state=enabled'
-alias sysd.ls.service.disabled='systemctl list-unit-files --type=service --state=disabled'
-alias sysd.ls.jobs='systemctl list-jobs'
-alias sysd.ls.timers='systemctl list-timers'
-alias sysd.ls.everything.loaded='systemctl list-units --all'
-alias sysd.ls.everything.installed='systemctl list-unit-files --all'
-# find
-alias sysd.find.service.loaded='systemctl list-units --type=service | rg'
-alias sysd.find.service.installed='systemctl list-unit-files --type=service | rg'
-# action
-alias sysd.restart='sudo systemctl restart'
-alias sysd.reload='sudo systemctl reload'
-alias sysd.start='sudo systemctl start'
-alias sysd.stop='sudo systemctl stop'
-alias sysd.enable='sudo systemctl enable'
-alias sysd.disable='sudo systemctl disable'
-alias sysd.reload-daemon='sudo systemctl daemon-reload'
-alias sysd.edit='sudo -E systemctl edit'
-# profile
+# - ls
+alias sysd.system.ls.service.loaded='systemctl list-units --type=service'
+alias sysd.system.ls.service.running='systemctl list-units --type=service --state=running'
+alias sysd.system.ls.service.failed='systemctl list-units --type=service --state=failed'
+alias sysd.system.ls.service='systemctl list-unit-files --type=service'
+alias sysd.system.ls.everything.loaded='systemctl list-units --all'
+alias sysd.system.ls.everything='systemctl list-unit-files --all'
+alias sysd.user.ls.service.loaded='systemctl --user list-units --type=service'
+alias sysd.user.ls.service.running='systemctl --user list-units --type=service --state=running'
+alias sysd.user.ls.service.failed='systemctl --user list-units --type=service --state=failed'
+alias sysd.user.ls.service='systemctl --user list-unit-files --type=service'
+alias sysd.user.ls.everything.loaded='systemctl --user list-units --all'
+alias sysd.user.ls.everything='systemctl --user list-unit-files --all'
+# - action
+alias sysd.system.restart='sudo systemctl restart'
+alias sysd.system.reload='sudo systemctl reload'
+alias sysd.system.start='sudo systemctl start'
+alias sysd.system.stop='sudo systemctl stop'
+alias sysd.system.enable='sudo systemctl enable'
+alias sysd.system.disable='sudo systemctl disable'
+alias sysd.system.edit='sudo -E systemctl edit'
+alias sysd.system.daemon-reload='sudo systemctl daemon-reload'
+alias sysd.user.restart='systemctl --user restart'
+alias sysd.user.reload='systemctl --user reload'
+alias sysd.user.start='systemctl --user start'
+alias sysd.user.stop='systemctl --user stop'
+alias sysd.user.enable='systemctl --user enable'
+alias sysd.user.disable='systemctl --user disable'
+alias sysd.user.edit='systemctl --user edit'
+alias sysd.user.daemon-reload='systemctl --user daemon-reload'
+# - profile
 alias sysd.profiling='systemd-analyze critical-chain'
 
 # journalctl
-alias sysd.logs='journalctl'
-alias sysd.logs-for='journalctl -u'
-sysd.logs.clear () {
+alias sysd.system.logs='journalctl'
+alias sysd.system.logs-for='journalctl -u'
+sysd.system.logs.clear () {
     sudo journalctl --rotate
     sudo journalctl --vacuum-time=1s
+}
+alias sysd.user.logs='journalctl --user'
+alias sysd.user.logs-for='journalctl --user -u'
+sysd.user.logs.clear () {
+    journalctl --user --rotate
+    journalctl --user --vacuum-time=1s
 }
