c5a02a6800e246abd31085eda0c9379d920d00a4
Quantum Snowball <quantum.snowball@gmail.com>
2026-08-11 23:24:18 +0800

    podman, direct copy from docker aliases

---
+++ utils/podman.zsh
@@ -0,0 +1,27 @@
+ensure podman || return
+
+
+# podman
+# ls
+alias podman.ls.containers='podman ps -a'
+alias podman.ls.images='podman images -a'
+alias podman.ls.images-tree='podman images -a --tree'
+alias podman.ls.volumes='podman volume ls'
+
+# interact
+alias podman.run='podman run'
+alias podman.run-interactive='podman run -it'
+alias podman.attach='podman attach'
+alias podman.start='podman start'
+alias podman.stop='podman stop'
+
+# remove
+alias podman.rm.image='podman rmi'
+alias podman.rm.container='podman rm'
+
+# cleanup
+alias podman.cleanup.containers='podman container prune'
+alias podman.cleanup.system='podman system prune'
+
+# sys
+alias podman.disk-usage='podman system df'
