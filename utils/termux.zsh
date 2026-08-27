f77dcdba051c9fe29479b3b8869894f41cb37862
Quantum Snowball <quantum.snowball@gmail.com>
2026-08-28 05:24:43 +0800

    should use termux as namespace

--- utils/termux.zsh
+++ utils/termux.zsh
@@ -2,10 +2,10 @@
 
 
 # some Termux common command listed here
-alias tm.wake='termux-wake-lock'
-alias tm.unwake='termux-wake-unlock'
-alias tm.setup-storage='termux-setup-storage'
-tm.theme () {
+alias termux.wake='termux-wake-lock'
+alias termux.unwake='termux-wake-unlock'
+alias termux.setup-storage='termux-setup-storage'
+termux.theme () {
     installed termux-style &&
         termux-style ||
         echo "Please installed termux-style at:\nhttps://github.com/adi1090x/termux-style"
@@ -18,15 +18,15 @@ termux.use-font() {
     ln -srf "$1" "$dst"
     termux-reload-settings
 }
-tm.nerd-fonts.hack () {
+termux.nerd-fonts.hack () {
     wget 'https://docs.google.com/uc?export=download&id=1OnxTvByrp24-qfW-4-lJDLZ2tvVOhfZT' -O ~/.termux/font.ttf
 }
-tm.nerd-fonts.hack-mono () {
+termux.nerd-fonts.hack-mono () {
     wget 'https://docs.google.com/uc?export=download&id=1cvetA3bgOCiXWoEhoAQevgkvvvbxBsiy' -O ~/.termux/font.ttf
 }
-tm.nerd-fonts.meslo () {
+termux.nerd-fonts.meslo () {
     wget 'https://docs.google.com/uc?export=download&id=1DUikjuAWKLS2zbt_ZysNLKwyv9SgywAZ' -O ~/.termux/font.ttf
 }
-tm.nerd-fonts.meslo-mono () {
+termux.nerd-fonts.meslo-mono () {
     wget 'https://docs.google.com/uc?export=download&id=1SQaqY3mRqrF897D2_RIur43hADURilNw' -O ~/.termux/font.ttf
 }
