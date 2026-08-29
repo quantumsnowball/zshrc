388813597ad8fb9a4462870334f407f3634cf480
Quantum Snowball <quantum.snowball@gmail.com>
2026-08-29 17:04:08 +0800

    color status text, better formatting, comments

--- utils/rclone.zsh
+++ utils/rclone.zsh
@@ -2,17 +2,20 @@ ensure rclone || return
 
 
 rclone.check-remotes() {
+    echo ""
     # run in a subshell to avoid printing the background job debug messages
     (
         # start multiple background jobs to connect to each client
-        for r in $(rclone listremotes); do
+        for name in $(rclone listremotes); do
             ( 
                 # basic test is to list the files with depth 1
-                rclone lsf "$r" --max-depth 1 &>/dev/null \
-                    && printf "%-25s SUCCESS\n" "$r" \
-                    || printf "%-25s FAILED\n" "$r" 
+                rclone lsf "$name" --max-depth 1 &>/dev/null \
+                    && printf "%-25s ${GREEN}SUCCESS${RESET}\n" "$name" \
+                    || printf "%-25s ${RED}FAILED${RESET}\n" "$name" 
             ) &
         done
+        # wait before all job done and exit
         wait
     )
+    echo ""
 }
