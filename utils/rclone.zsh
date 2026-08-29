d3fa13fb0a267949fcef266a5035d377130aad30
Quantum Snowball <quantum.snowball@gmail.com>
2026-08-29 16:58:28 +0800

    run in parallel background jobs

--- utils/rclone.zsh
+++ utils/rclone.zsh
@@ -2,9 +2,17 @@ ensure rclone || return
 
 
 rclone.check-remotes() {
-    for r in $(rclone listremotes); do
-        rclone lsf "$r" --max-depth 1 &>/dev/null \
-            && printf "%-25s SUCCESS\n" "$r" \
-            || printf "%-25s FAILED\n" "$r"
-    done
+    # run in a subshell to avoid printing the background job debug messages
+    (
+        # start multiple background jobs to connect to each client
+        for r in $(rclone listremotes); do
+            ( 
+                # basic test is to list the files with depth 1
+                rclone lsf "$r" --max-depth 1 &>/dev/null \
+                    && printf "%-25s SUCCESS\n" "$r" \
+                    || printf "%-25s FAILED\n" "$r" 
+            ) &
+        done
+        wait
+    )
 }
