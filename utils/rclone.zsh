8a4bb8e4be1c316bfec694d2fa96771665c6c4fe
Quantum Snowball <quantum.snowball@gmail.com>
2026-08-29 16:42:04 +0800

    don't be too confusing

--- utils/rclone.zsh
+++ utils/rclone.zsh
@@ -1,15 +1,3 @@
 ensure rclone || return
 
 
-# rclone
-alias rc="rclone"
-
-alias rc.list-remotes="rclone listremotes"
-alias rclist=rc.list-remotes
-
-alias rc.ls="rclone ls"
-alias rcls=rc.ls
-
-alias rc.lsd="rclone lsd"
-alias rclsd=rc.lsd
-
