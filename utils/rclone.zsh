bcf259395d9a23c10e029104d8eb8c65bd87f7fa
Quantum Snowball <quantum.snowball@gmail.com>
2026-08-29 16:48:55 +0800

    rclone.check-remotes
    a util that try to connect to each remote to see if still working

--- utils/rclone.zsh
+++ utils/rclone.zsh
@@ -1,3 +1,10 @@
 ensure rclone || return
 
 
+rclone.check-remotes() {
+    for r in $(rclone listremotes); do
+        rclone lsf "$r" --max-depth 1 &>/dev/null \
+            && printf "%-25s SUCCESS\n" "$r" \
+            || printf "%-25s FAILED\n" "$r"
+    done
+}
