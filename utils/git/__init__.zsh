5d6d156ad709f52b80921a4f7a4d50bb223152e8
Quantum Snowball <quantum.snowball@gmail.com>
2026-08-31 16:41:54 +0800

    git clone

--- utils/git/__init__.zsh
+++ utils/git/__init__.zsh
@@ -16,52 +16,5 @@ source $XDG_CONFIG_HOME/zshrc/utils/git/log.zsh
 # config
 source $XDG_CONFIG_HOME/zshrc/utils/git/config.zsh
 
-#helpers
-gh.clone-my-repo() {
-    # default use https url
-    local use_ssh=false
-    local use_token=false
-    local help="Usage: gh.clone-my-repo [-ssh|-rw|-token] <repo> [<git clone args>]"
-
-    # if -rw or -ssh flag active, use ssh url
-    # only machine with authorized private key installed can write to repo
-    if [ "$1" = "-ssh" ] || [ "$1" = "-rw" ]; then
-        use_ssh=true
-        shift
-    # if -token flag active, use https with username prefixed domain
-    elif [ "$1" = "-token" ]; then
-        # NOTE: 
-        # to clone private repo, create the token with Depository access: All repositories
-        # also at least grant Repository permissions > Contents > Access: Read-only
-        use_token=true
-        shift
-    elif [[ "$1" == -* ]]; then
-        echo "Error: invalid flag"
-        echo $help
-        return 1 
-    fi
-
-    # the arg that follows is always the repo name
-    if [ -z "$1" ]; then
-        echo "Error: please provide repo name to clone"
-        echo $help
-        return 1
-    fi
-    local repo="$1"
-    shift
-
-    # after the repo name, all following args ($@) are passed onto git clone
-    if $use_ssh; then
-        # echo "git clone $myghssh/$repo.git $@"
-        git clone "$myghssh/$repo.git" "$@"
-    elif $use_token; then
-        echo -n "GitHub personal access token: "
-        read -s token
-        git clone "https://$myghname:$token@github.com/$myghname/$repo.git" "$@"
-    else
-        # echo "git clone $mygh/$repo.git $@"
-        git clone "$mygh/$repo.git" "$@"
-    fi
-}
-
-
+# clone
+source $XDG_CONFIG_HOME/zshrc/utils/git/clone.zsh
