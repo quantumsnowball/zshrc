3f6aa1749dc039105bfb8a88d87ffd31d562883a
Quantum Snowball <quantum.snowball@gmail.com>
2026-08-31 16:42:45 +0800

    cleanup unused

--- utils/git/__init__.zsh
+++ utils/git/__init__.zsh
@@ -1,20 +1,8 @@
-# constant
-export gh=https://github.com
-export myghname=quantumsnowball
-export mygh=$gh/$myghname
-export myghssh=git@github.com:$myghname
-
-
 ensure git || return
 
 # pager
 export GIT_PAGER="less"
 
-# git log
 source $XDG_CONFIG_HOME/zshrc/utils/git/log.zsh
-
-# config
 source $XDG_CONFIG_HOME/zshrc/utils/git/config.zsh
-
-# clone
 source $XDG_CONFIG_HOME/zshrc/utils/git/clone.zsh
