aea2aedefd846ef6e49dfc37511eb504c178710b
Quantum Snowball <quantum.snowball@gmail.com>
2026-08-31 16:36:39 +0800

    move to github

--- utils/git/__init__.zsh
+++ utils/git/__init__.zsh
@@ -144,23 +144,3 @@ gh.clone-my-repo() {
 }
 
 
-ensure gh || return
-
-
-gh.search-my-repo() {
-    # use gh cli helper, need a read-only api token
-    gh search repos \
-        --owner=quantumsnowball \
-        --sort=updated \
-        --order=desc \
-        --limit=20 \
-        --json name,language,visibility,updatedAt \
-        --template '{{range .}}{{
-            tablerow 
-            (.name | autocolor "white") 
-            (printf "%.7s" .language | autocolor "yellow") 
-            (.visibility | autocolor "cyan") 
-            (printf "%.9s" (timeago .updatedAt) | autocolor "green")
-        }}{{end}}' \
-        "$@"
-}
