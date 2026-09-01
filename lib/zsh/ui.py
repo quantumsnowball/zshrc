1d7865ec4d0a4f0bb11f2b2be1e975bd8d43fc3a
Quantum Snowball <quantum.snowball@gmail.com>
2026-09-02 02:11:30 +0800

    comments, tidy up

--- lib/zsh/ui.py
+++ lib/zsh/ui.py
@@ -13,34 +13,35 @@ class Cell(Protocol):
 class LiveTable:
     def __init__(
         self,
-        # cells is a 2d sequence, cells[row][column] structure
+        # cells, 2d sequence, cells[row][column] structure
         cells: list[Sequence[Cell]],
         *,
-        # stub column
+        # stub column (the left most column)
         stub_header: RenderableType = '',
         stubs: Sequence[RenderableType] | None = None,
-        # cell columns
-        # - if cell_columns is provided, will override related settings
+        stub_column_kwargs: Mapping[str, Any] | None = None,
+        # cell columns (all data columns)
         cell_columns: Sequence[Mapping[str, Any]] | None = None,
         cell_headers: Sequence[RenderableType] = (),
-        # kwargs
-        stub_column_kwargs: Mapping[str, Any] | None = None,
         cell_column_kwargs: Mapping[str, Any] | None = None,
+        # rich
         table_kwargs: Mapping[str, Any] | None = None,
         live_kwargs: Mapping[str, Any] | None = None,
     ) -> None:
         # data
         self._cells = cells
+        # stub column
         self._stub_header = stub_header
         self._stubs = stubs
+        self._stub_column_kwargs = stub_column_kwargs or {}
+        # cell columns
         self._cell_columns = cell_columns
         self._cell_headers = cell_headers
-        # kwargs
-        self._stub_column_kwargs = stub_column_kwargs or {}
         self._cell_column_kwargs = cell_column_kwargs or {}
+        # kwargs
         self._table_kwargs = table_kwargs or {}
         self._live_kwargs = live_kwargs or {}
-        # rich live
+        # rich
         self._live = Live(self._renderer(), **self._live_kwargs)
 
     @property
@@ -55,14 +56,16 @@ class LiveTable:
         self._live.__exit__(*_)
 
     def _renderer(self) -> Table:
-        # create columns
+        # create column objects
         columns = [
             *(
+                # if stubs is not provided, there will be no stub column
                 (Column(self._stub_header, **self._stub_column_kwargs), )
                 if self._stubs is not None else
                 ()
             ),
             *(
+                # if cell_columns is provided, will use that to create invidual Column, ignoring other related settings
                 (Column(**kw) for kw in self._cell_columns)
                 if self._cell_columns is not None else
                 (Column(cell_header, **self._cell_column_kwargs) for cell_header in self._cell_headers)
@@ -77,8 +80,7 @@ class LiveTable:
                 *(cell.text for cell in row_cells),
             ]
             table.add_row(*texts)
-
-        # Table object giving back for render
+        # return new Table object to live
         return table
 
     def update(self) -> None:
