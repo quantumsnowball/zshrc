2ee9464c05c15265fa455712e2cbbfca3616fc87
Quantum Snowball <quantum.snowball@gmail.com>
2026-09-02 20:00:39 +0800

    Result knows how to append a SubtitleStream

--- utils/ffprobe/lib/ffprobe_streams/result/__init__.py
+++ utils/ffprobe/lib/ffprobe_streams/result/__init__.py
@@ -1,8 +1,7 @@
-from typing import List
-
 from ffprobe_streams.result.format import Format
 from ffprobe_streams.result.lib.types import Data
 from ffprobe_streams.result.stream.audio import AudioStream
+from ffprobe_streams.result.stream.subtitle import SubtitleStream
 from ffprobe_streams.result.stream.video import VideoStream
 
 
@@ -11,13 +10,16 @@ class Result:
         self._d = data
 
     @property
-    def streams(self) -> list[VideoStream | AudioStream]:
-        streams: List[VideoStream | AudioStream] = []
+    def streams(self) -> list[VideoStream | AudioStream | SubtitleStream]:
+        streams: list[VideoStream | AudioStream | SubtitleStream] = []
         for s in self._d['streams']:
-            if s["codec_type"] == "video":
+            if s['codec_type'] == 'video':
                 streams.append(VideoStream(s))
-            elif s["codec_type"] == "audio":
+            elif s['codec_type'] == 'audio':
                 streams.append(AudioStream(s))
+            elif s['codec_type'] == 'subtitle':
+                streams.append(SubtitleStream(s))
+
         return streams
 
     @property
