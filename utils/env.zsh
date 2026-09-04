65ab101cfaa6223343b8d56aad7ee5fb1faa23b1
Quantum Snowball <quantum.snowball@gmail.com>
2026-09-04 23:26:19 +0800

    dynamically create namespaces for env utils functions, indeed this is very good pattern

--- utils/env.zsh
+++ utils/env.zsh
@@ -1,24 +1,12 @@
-# env vars
-alias zsh.env-vars='env | bat -l sh'
-alias lsenv=zsh.env-vars
-alias lsenvs=zsh.env-vars
+() {
+    # namespaces
+    local ns=(zsh sh shell env vars sys os)
 
-alias zsh.shell-vars='set | bat -l sh'
-alias lsvar=zsh.shell-vars
-alias lsvars=zsh.shell-vars
-alias lsenvall=zsh.shell-vars
-
-alias zsh.aliases='alias | bat -l sh'
-alias lsalias=zsh.aliases
-
-alias zsh.paths='printf "%s\n" $path | bat -l python'
-alias lspath=zsh.paths
-alias lspaths=zsh.paths
-
-alias zsh.function-paths='printf "%s\n" $fpath | bat -l python'
-alias lsfpath=zsh.function-paths
-alias lsfpaths=zsh.function-paths
-
-alias zsh.exports='export | bat -l sh'
-alias lsexport=zsh.exports
-alias lsexports=zsh.exports
+    # env vars
+    alias ${^ns}.env-vars='env | bat -l sh'
+    alias ${^ns}.shell-vars='set | bat -l sh'
+    alias ${^ns}.aliases='alias | bat -l sh'
+    alias ${^ns}.paths='printf "%s\n" $path | bat -l python'
+    alias ${^ns}.function-paths='printf "%s\n" $fpath | bat -l python'
+    alias ${^ns}.exports='export | bat -l sh'
+}
