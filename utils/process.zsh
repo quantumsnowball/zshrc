9e277bf0d5c21769fadb681c70c8a61e20216ece
Quantum Snowball <quantum.snowball@gmail.com>
2026-09-05 00:07:57 +0800

    process namesapces

--- utils/process.zsh
+++ utils/process.zsh
@@ -1,7 +1,9 @@
-# process tree info
-alias ps.process='ps -eHo pid,comm'
-alias lsps=ps.process
-alias ps.process-tree='pstree -p | bat'
-alias lsps2=ps.process-tree
-alias ps.highlight='lsps | rg -B2 -A1'
-alias findps=ps.highlight
+() {
+    # namespaces
+    local ns=(ps proc process task shell sh sys os)
+
+    # process tree info
+    alias ${^ns}.process='ps -eHo pid,comm'
+    alias ${^ns}.process-tree='pstree -p | bat'
+    alias ${^ns}.process-search-and-highlight='ps.process | rg -B2 -A1'
+}
