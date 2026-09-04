b0073e63d4e152e2bfd1997e832958cf0a79f695
Quantum Snowball <quantum.snowball@gmail.com>
2026-09-04 14:18:24 +0800

    termux backup and restore script

--- utils/termux.zsh
+++ utils/termux.zsh
@@ -18,3 +18,23 @@ termux.use-font() {
     ln -srf "$1" "$dst"
     termux-reload-settings
 }
+
+# backup and restore
+termux.backup.backup-home-and-usr-dir-to() {
+    if [ "$#" -ne 1 ]; then
+        echo "Usage: ${funcstack[1]} <path-to-archive>.tar[.gz|.xz]" >&2
+        return 1
+    fi
+    # ensure the backup dir exists
+    mkdir -p "$(dirname "$1")"
+    # do a tar -acf to archive home/ and usr/ to a file (.tar, .tar.gz or .tar.xz)
+    tar --auto-compress --create --file="$1" --directory=/data/data/com.termux/files ./home ./usr
+}
+termux.backup.restore-home-and-usr-dir-from() {
+    if [ "$#" -ne 1 ] || [ ! -f "$1" ]; then
+        echo "Usage: ${funcstack[1]} <path-to-archive>.tar[.gz|.xz]" >&2
+        return 1
+    fi
+    # do a tar -xf to extract an archive file (.tar, .tar.gz or .tar.xz) to restore the home/ and usr/ into files/
+    tar --extract --file="$1" --directory=/data/data/com.termux/files --recursive-unlink --preserve-permissions
+}
