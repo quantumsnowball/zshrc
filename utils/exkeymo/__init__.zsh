9aff4dae9e00d2736f0debeae717d54e1355facc
Quantum Snowball <quantum.snowball@gmail.com>
2026-09-05 21:49:04 +0800

    exkeymo.use-kcm-layout, compile and prompt for installion in one go

--- utils/exkeymo/__init__.zsh
+++ utils/exkeymo/__init__.zsh
@@ -12,3 +12,24 @@ exkeymo.compile-kcm-to-apk() {
     # pass any args into compile.js
     node "$XDG_CONFIG_HOME/zshrc/utils/exkeymo/compiler/main.js" "$@"
 }
+
+exkeymo.use-kcm-layout() {
+    local kcm_file="$1"
+    # local output_apk="$TMPDIR/exkeymo.apk"
+    local output_apk="$HOME/storage/downloads/exkeymo.apk"
+
+    if [[ -z "$kcm_file" ]]; then
+        echo "Usage: Exkeymo.compile-and-use-kcm-layout <input.kcm>" >&2
+        return 1
+    fi
+
+    # Compile the layout to /tmp/output.apk
+    exkeymo.compile-kcm-to-apk "$kcm_file" "$output_apk" || return 1
+
+    # Trigger Android's package installer interface
+    if [[ -f "$output_apk" ]]; then
+        echo "Launching Android package installer..."
+        termux-open "$output_apk"
+    fi
+}
+
