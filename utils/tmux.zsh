4ffd68e13626ec8ea97a271121c25ac1b3945835
Quantum Snowball <quantum.snowball@gmail.com>
2026-09-06 03:31:27 +0800

    `t` as a smart tmux attach / create helper function

--- utils/tmux.zsh
+++ utils/tmux.zsh
@@ -1,8 +1,24 @@
 ensure tmux || return
 
 
-# short hand
-alias t='tmux attach -t 0 || tmux new -s 0'
+# smart tmux attach / create helper function
+t() {
+    # us $1 as target name, default ot main
+    local target="${1:-main}"
+
+    # inside tmux
+    if [[ -n "$TMUX" ]]; then
+        # create target session if missing
+        tmux has-session -t "$target" 2>/dev/null || tmux new-session -d -s "$target"
+        # then switch to it
+        tmux switch-client -t "$target"
+    # outside tmux
+    else
+        # attach to target session, or create it if missing
+        tmux new-session -A -s "$target"
+    fi
+}
+
 
 () {
     # namespaces
