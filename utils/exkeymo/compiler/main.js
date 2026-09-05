d55c5c2bdb01b0110e216c451c51c897d8ddcb2d
Quantum Snowball <quantum.snowball@gmail.com>
2026-09-05 20:21:31 +0800

    SUCCESS: compiled a perfect apk, installed successfully!

--- utils/exkeymo/compiler/main.js
+++ utils/exkeymo/compiler/main.js
@@ -2,8 +2,6 @@
 
 import fs from 'node:fs/promises';
 import { buildApk } from './builder.js';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 
 const args = process.argv.slice(2);
 const inputFilePath = args[0];
@@ -14,26 +12,13 @@ if (!inputFilePath) {
     process.exit(1);
 }
 
-try {
-    const inputFileContent = await fs.readFile(inputFilePath, 'utf-8');
+const inputFileContent = await fs.readFile(inputFilePath, 'utf-8');
 
-    console.log(inputFileContent);
+console.log('Compiling APK...');
+const apkBytes = await buildApk(inputFileContent, null);
 
-    console.log(outputFilePath)
+console.log(`Writing APK to: ${outputFilePath}`);
+await fs.writeFile(outputFilePath, Buffer.from(apkBytes));
 
-    console.log(buildApk)
-
-    // console.log('Compiling APK...');
-    // const apkBytes = await buildApk(inputFileContent, null);
-
-    // console.log(`Writing APK to: ${outputFilePath}`);
-    // await fs.writeFile(outputFilePath, Buffer.from(apkBytes));
-
-    // console.log('Build complete!');
-
-} catch (err) {
-    console.error(`Error reading file "${inputFilePath}":`, err.message);
-    process.exit(1);
-}
-
+console.log('Build complete!');
 
