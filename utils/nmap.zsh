e4737b570b54f0358dfa24c92d0e476a731a1c16
Quantum Snowball <quantum.snowball@gmail.com>
2026-09-06 16:07:06 +0800

    cleanup

--- utils/nmap.zsh
+++ utils/nmap.zsh
@@ -4,15 +4,6 @@ ensure nmap || return
 : "${NMAP_DEFAULT_SUBNET:=192.168.1.0/24}"
 : "${NMAP_DEFAULT_DNS:=192.168.1.1}"
 
-# Shared Output Helper
-# nmap.parser() {
-#     printf "\033[33m%-15s %s\033[0m\n" 'IP Address' 'Name'
-#     grep "^Nmap scan report for" \
-#         | awk '{print $5, $(NF)}' \
-#         | grep -v '^192\.168' \
-#         | awk '{gsub(/[()]/, ""); printf "\033[32m%-15s\033[0m %s\n", $2, $1}'
-# }
-
 # ==============================================================================
 # Discover
 # ==============================================================================
