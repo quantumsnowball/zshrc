46f1a93cf5992104823d818d1f714d93e7019614
Quantum Snowball <quantum.snowball@gmail.com>
2026-09-07 17:26:42 +0800

    pkg aliases

---
+++ utils/pkg.zsh
@@ -0,0 +1,14 @@
+installed pkg || return
+
+
+alias pkg.autoclean='pkg autoclean'
+alias pkg.install='pkg install'
+alias pkg.search='pkg search'
+alias pkg.list-all='pkg list-all'
+alias pkg.list-installed='pkg list-installed'
+alias pkg.show='pkg show'
+alias pkg.files='pkg files'
+alias pkg.reinstall='pkg reinstall'
+alias pkg.uninstall='pkg uninstall'
+alias pkg.update='pkg update'
+alias pkg.upgrade='pkg upgrade'
