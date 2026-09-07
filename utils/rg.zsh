05c202601ac7adc2842e5739b846d10351a1e5ab
Quantum Snowball <quantum.snowball@gmail.com>
2026-09-07 17:56:35 +0800

    rg common context aliases

--- utils/rg.zsh
+++ utils/rg.zsh
@@ -17,3 +17,14 @@ rg.less () {
         --color=Nk \
 }
 alias rg.hidden.less='rg.less --hidden'
+
+# context
+() {
+    # common multiples
+    local n
+    for n (01 02 04 08 16 32 64); do
+        alias rg.context.${n}-lines="rg -C${n}"
+        alias rg.context.${n}-lines.before="rg -B${n}"
+        alias rg.context.${n}-lines.after="rg -A${n}"
+    done
+}
