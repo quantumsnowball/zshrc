4ec5ff568423e5447cdf8fb6a592b76b4390dfcf
Quantum Snowball <quantum.snowball@gmail.com>
2026-09-09 01:20:31 +0800

    don't need anymore

--- utils/exkeymo/__init__.zsh
+++ utils/exkeymo/__init__.zsh
@@ -2,13 +2,6 @@ ensure node || return
 ensure apktool || return
 
 
-exkeymo.start-local-builder-server() {
-    (
-        cd "$HOME/Projects/exkeymo/"
-        python -m http.server 8080
-    )
-}
-
 exkeymo.compile-kcm-to-apk() {
     # pass any args into compile.js
     node "$XDG_CONFIG_HOME/zshrc/utils/exkeymo/compiler/main.js" "$@"
