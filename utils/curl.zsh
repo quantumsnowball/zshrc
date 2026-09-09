4efca9bbb69dd32f122450b43b8e0edc8856ba2d
Quantum Snowball <quantum.snowball@gmail.com>
2026-09-09 23:32:49 +0800

    curl.save-url-to-file, curl.save-url-to-directory

--- utils/curl.zsh
+++ utils/curl.zsh
@@ -27,3 +27,17 @@ ip.public.where-am-i () {
     alias ${^ns}.public.addr='ip.public.addr'
     alias ${^ns}.public.where-am-i='ip.public.where-am-i'
 }
+
+# helpers
+curl.save-url-to-file() {
+    [[ -n "$1" && -n "$2" ]] || { echo "Usage: $0 <url> <output_file>" >&2; return 1; }
+    [[ -d "${2:h}" ]] || { echo "Error: Directory '${2:h}' does not exist." >&2; return 1; }
+    curl --fail --location --output "$2" "$1"
+}
+
+curl.save-url-to-directory() {
+    [[ -n "$1" && -n "$2" ]] || { echo "Usage: $0 <url> <output_dir>" >&2; return 1; }
+    [[ -d "$2" ]] || { echo "Error: Directory '$2' does not exist." >&2; return 1; }
+    curl --fail --location --remote-name --output-dir "$2" "$1"
+}
+
