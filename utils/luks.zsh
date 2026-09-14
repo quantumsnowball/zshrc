291c63aebf1bc11da6c3b4b84ed8dffed6ea3b31
Quantum Snowball <quantum.snowball@gmail.com>
2026-09-14 20:38:30 +0800

    luks.drive.remove-key

--- utils/luks.zsh
+++ utils/luks.zsh
@@ -5,11 +5,15 @@ luks.drives.list-encrypted() {
     luks.drives | grep crypto_LUKS
 }
 luks.drive.list-keyslots() {
-    [[ -b "$1" ]] || { echo "device $1 does not exists"; return 1 }
+    [[ -b "$1" ]] || { echo "device $1 does not exists"; return 1; }
     sudo cryptsetup luksDump "$1"
 }
 luks.drive.add-key() {
-    [[ -b "$1" ]] || { echo "device $1 does not exists"; return 1 }
+    [[ -b "$1" ]] || { echo "device $1 does not exists"; return 1; }
     sudo cryptsetup luksAddKey "$1"
+}
+luks.drive.remove-key() {
+    [[ -b "$1" ]] || { echo "device $1 does not exists"; return 1; }
+    sudo cryptsetup luksRemoveKey "$1"
 }
 
