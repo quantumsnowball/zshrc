2b7e9780aa6b48fe05fc10c8e1ea0d5280d448cc
Quantum Snowball <quantum.snowball@gmail.com>
2026-09-14 20:33:27 +0800

    should check if input is a block device

--- utils/luks.zsh
+++ utils/luks.zsh
@@ -5,6 +5,7 @@ luks.drives.list-encrypted() {
     luks.drives | grep crypto_LUKS
 }
 luks.drive.list-keyslots() {
+    [[ -b "$1" ]] || { echo "device $1 does not exists"; return 1 }
     sudo cryptsetup luksDump "$1"
 }
 
