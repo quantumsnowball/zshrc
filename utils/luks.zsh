4188cc4ab4b0a249464c4f8d2a68c02f74b2d176
Quantum Snowball <quantum.snowball@gmail.com>
2026-09-14 20:40:02 +0800

    simplify deps

--- utils/luks.zsh
+++ utils/luks.zsh
@@ -2,7 +2,7 @@ luks.drives() {
     lsblk -p -o NAME,FSTYPE,SIZE,MOUNTPOINTS,UUID
 }
 luks.drives.list-encrypted() {
-    luks.drives | grep crypto_LUKS
+    lsblk -p -o NAME,FSTYPE,SIZE,MOUNTPOINTS,UUID | grep crypto_LUKS
 }
 luks.drive.list-keyslots() {
     [[ -b "$1" ]] || { echo "device $1 does not exists"; return 1; }
