caa608bef22f712c9744eddb063043aadd31d3bc
Quantum Snowball <quantum.snowball@gmail.com>
2026-09-15 19:46:30 +0800

    openrgb static color shortcuts

---
+++ utils/openrgb.zsh
@@ -0,0 +1,12 @@
+ensure openrgb || return
+
+
+# openrgb static color shortcuts
+openrgb.black()   { openrgb --mode static --color 000000 }
+openrgb.white()   { openrgb --mode static --color FFFFFF }
+openrgb.red()     { openrgb --mode static --color FF0000 }
+openrgb.green()   { openrgb --mode static --color 00FF00 }
+openrgb.blue()    { openrgb --mode static --color 0000FF }
+openrgb.yellow()  { openrgb --mode static --color FFFF00 }
+openrgb.cyan()    { openrgb --mode static --color 00FFFF }
+openrgb.magenta() { openrgb --mode static --color FF00FF }
