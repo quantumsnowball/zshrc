5a5a9f5467f8eeb7c3b68d88acbab0b334eaf4be
Quantum Snowball <quantum.snowball@gmail.com>
2026-09-20 14:12:24 +0800

    luks.list-keys

--- utils/luks.zsh
+++ utils/luks.zsh
@@ -30,8 +30,9 @@ luks.test-passphrase() {
         echo "Passphrase is CORRECT!" || echo "Passphrase is WRONG!"
 }
 luks.list-keys() {
-    [[ -n "$1" && -b "$1" ]] || { echo "Usage: $0 <valid block device path>"; return 1; }
-    sudo cryptsetup luksDump "$1"
+    local target="$(luks.resolve-label "${1}")"
+    [[ -n "$target" && -b "$target" ]] || { echo "Usage: $0 <valid block device path or partlabel>"; return 1; }
+    sudo cryptsetup luksDump "$target"
 }
 luks.list-slots() {
     [[ -n "$1" && -b "$1" ]] || { echo "Usage: $0 <valid block device path>"; return 1; }
