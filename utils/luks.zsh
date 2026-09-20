8c1fab40e2b7069e26aeda07a35c746642a45460
Quantum Snowball <quantum.snowball@gmail.com>
2026-09-20 16:24:43 +0800

    ensure non-empty

--- utils/luks.zsh
+++ utils/luks.zsh
@@ -90,7 +90,7 @@ luks.list-keyfiles() {
     sudo find /etc/cryptsetup-keys.d/ -type f -exec md5sum {} + 2>/dev/null
 }
 luks.generate-keyfile() {
-    [[ -e "/dev/disk/by-partlabel/$1" ]] || { echo "Usage: $0 <partlabel>" >&2; return 1; }
+    [[ -n "$1" && -e "/dev/disk/by-partlabel/$1" ]] || { echo "Usage: $0 <partlabel>" >&2; return 1; }
     sudo dd if=/dev/urandom of="/etc/cryptsetup-keys.d/$1.key" bs=1024 count=4 status=none && sudo chmod 400 "/etc/cryptsetup-keys.d/$1.key"
     sudo md5sum /etc/cryptsetup-keys.d/$1.key
 }
