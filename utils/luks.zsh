b0620108cbcbeb4680ae15a747e618d11ca95033
Quantum Snowball <quantum.snowball@gmail.com>
2026-09-20 15:47:45 +0800

    todo

--- utils/luks.zsh
+++ utils/luks.zsh
@@ -82,6 +82,7 @@ luks.enable-auto-unlock.by-tpm2() {
     echo "done! reboot to test auto-unlock"
 }
 luks.set-reserved-blocks-percentage-to-zero() {
+    # TODO: resolve the label to a mapper
     [[ -n "$1" && -b "$1" && "$1" == /dev/mapper/* ]] || { echo "usage: $0 <valid /dev/mapper/ block device path>"; return 1; }
     sudo tune2fs -m 0 "$1"
 }
