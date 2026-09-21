b65da673529daaa72b5ae07cea23dbdd88108f26
Quantum Snowball <quantum.snowball@gmail.com>
2026-09-21 15:05:49 +0800

    rclone.copy, rclone.copy-1t, and their preview variants

--- utils/rclone.zsh
+++ utils/rclone.zsh
@@ -33,3 +33,22 @@ rclone.remote.check-sftp() { rclone.remote.check sftp }
 rclone.remote.list() { rclone listremotes }
 rclone.remote.list-sftp() { rclone listremotes --type sftp }
 rclone.remote.list-drive() { rclone listremotes --type drive }
+
+# copy
+rclone.copy() {
+    # ensure src and dst exists
+    (( $# < 2 )) && { echo "usage: rclone.copy <src> <dst> [flags...]" >&2; return 1; }
+    local src="$1" dst="$2"; shift 2
+    # default to verbose output and show progress
+    rclone copy -vP "$src" "$dst" "$@"
+}
+rclone.copy.preview() {
+    rclone.copy "$@" --dry-run
+}
+# copy (sequentially)
+rclone.copy-1t() {
+    rclone.copy "$@" --transfers=1
+}
+rclone.copy-1t.preview() {
+    rclone.copy "$@" --transfers=1 --dry-run
+}
