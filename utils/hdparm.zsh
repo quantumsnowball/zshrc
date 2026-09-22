f5f827c50a2e875b0c8aa5115d75b17d8deaae53
Quantum Snowball <quantum.snowball@gmail.com>
2026-09-23 03:02:43 +0800

    better code style

--- utils/hdparm.zsh
+++ utils/hdparm.zsh
@@ -2,39 +2,26 @@ installed hdparm || return
 
 
 hdparm.power-state() {
-    # if no args are supplied, detect and check all physical disks
-    if [[ $# -eq 0 ]]; then
-        local -a drives
-        drives=($(lsblk -d -n -o PATH,TYPE | awk '$2=="disk" && $1 ~ /\/dev\/sd/ {print $1}'))
+    # if args supplied, execute directly
+    (( $# > 0 )) && { sudo hdparm -C "$@"; return; }
 
-        if [[ ${#drives} -eq 0 ]]; then
-            echo "no disk drives found"
-            return 1
-        fi
-
-        sudo hdparm -C "${drives[@]}"
-
-    # otherwise just pass all the args
-    else
-        sudo hdparm -C "$@"
-    fi
+    # otherwise, auto-detect physical disks and execute
+    local -a drives=($(lsblk -d -n -o PATH,TYPE | awk '$2=="disk" && $1 ~ /\/dev\/sd/ {print $1}'))
+    (( ${#drives} == 0 )) && { echo "no drives found"; return 1; }
+    sudo hdparm -C "${drives[@]}"
 }
+
 hdparm.spin-down() {
-    # if no args are supplied, spin down all physical SATA disks
-    if [[ $# -eq 0 ]]; then
-        local -a drives
-        drives=($(lsblk -d -n -o PATH,TYPE | awk '$2=="disk" && $1 ~ /\/dev\/sd/ {print $1}'))
-
-        if [[ ${#drives} -eq 0 ]]; then
-            echo "no HDD drives found"
-            return 1
-        fi
-
-        sudo hdparm -y "${drives[@]}"
-    else
+    # spin down specified or auto-detected disks
+    if (( $# > 0 )); then
         sudo hdparm -y "$@"
+    else
+        local -a drives=($(lsblk -d -n -o PATH,TYPE | awk '$2=="disk" && $1 ~ /\/dev\/sd/ {print $1}'))
+        (( ${#drives} == 0 )) && { echo "no HDD drives found"; return 1; }
+        sudo hdparm -y "${drives[@]}"
     fi
 
+    # display states result after
     echo "\nDrive states:"
     hdparm.power-state "$@"
 }
