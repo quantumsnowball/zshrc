2fbe98b583b412e5394fbac188a123d046ac1f56
Quantum Snowball <quantum.snowball@gmail.com>
2026-09-23 02:50:48 +0800

    hdparm.power-state, hdparm.spin-down

---
+++ utils/hdparm.zsh
@@ -0,0 +1,26 @@
+installed hdparm || return
+
+
+hdparm.power-state() {
+    # if no args are supplied, detect and check all physical disks
+    if [[ $# -eq 0 ]]; then
+        local -a drives
+        drives=($(lsblk -d -n -o PATH,TYPE | awk '$2=="disk" && $1 ~ /\/dev\/sd/ {print $1}'))
+
+        if [[ ${#drives} -eq 0 ]]; then
+            echo "no disk drives found"
+            return 1
+        fi
+
+        sudo hdparm -C "${drives[@]}"
+
+    # otherwise just pass all the args
+    else
+        sudo hdparm -C "$@"
+    fi
+}
+hdparm.spin-down() {
+    sudo hdparm -y "$1"
+    echo "\n\nDrive states:"
+    hdparm.current-power-state 
+}
